32f8f72e62c2ef0ac9658578ef6afe9e2e8ab7b0
Eugene Liu <eugene.liu@intel.com>
2024-11-25 12:26:51 +0000

    Add 2.5.1 PyTorch & Torchvision (#4128)
    upgrade torch version

--- src/otx/cli/utils/installation.py
+++ src/otx/cli/utils/installation.py
@@ -32,6 +32,7 @@ AVAILABLE_TORCH_VERSIONS = {
     "2.3.0": {"torchvision": "0.18.0", "cuda": ("11.8", "12.1")},
     "2.3.1": {"torchvision": "0.18.1", "cuda": ("11.8", "12.1")},
     "2.4.0": {"torchvision": "0.19.0", "cuda": ("11.8", "12.1", "12.4")},
+    "2.5.1": {"torchvision": "0.20.1", "cuda": ("11.8", "12.1", "12.4")},
 }
 
 MM_REQUIREMENTS = [
