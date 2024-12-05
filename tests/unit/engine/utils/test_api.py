1f51ee5ac2e9fa9c7a7c66bae61529b4eb9676ba
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2024-12-05 13:34:47 +0100

    Hide semi-sl recipes from tests (#4144)
    * Hide semi sl recipes from tests
    
    * Fix unit tests

--- tests/unit/engine/utils/test_api.py
+++ tests/unit/engine/utils/test_api.py
@@ -28,15 +28,11 @@ def test_list_models_pattern() -> None:
 
     target = [
         "efficientnet_b0",
-        "efficientnet_b0_semisl",
         "efficientnet_v2",
-        "efficientnet_v2_semisl",
         "maskrcnn_efficientnetb2b",
         "maskrcnn_efficientnetb2b_tile",
         "tv_efficientnet_b3",
-        "tv_efficientnet_b3_semisl",
         "tv_efficientnet_v2_l",
-        "tv_efficientnet_v2_l_semisl",
     ]
     assert sorted(models) == sorted(target)
 
