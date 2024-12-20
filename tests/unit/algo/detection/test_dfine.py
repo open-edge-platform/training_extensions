dff6ecc72cfd13c84378808871e14f09d9a2d4c5
Eugene Liu <eugene.liu@intel.com>
2024-12-20 15:25:33 +0000

    Update d-fine unit test

--- tests/unit/algo/detection/test_dfine.py
+++ tests/unit/algo/detection/test_dfine.py
@@ -9,13 +9,62 @@ import pytest
 import torch
 import torchvision
 from otx.algo.detection.backbones.hgnetv2 import HGNetv2
+from otx.algo.detection.d_fine import DFine
 from otx.algo.detection.heads.dfine_decoder import DFINETransformer
 from otx.algo.detection.losses.dfine_loss import DFINECriterion
 from otx.algo.detection.necks.dfine_hybrid_encoder import HybridEncoder
 from otx.algo.detection.rtdetr import DETR
+from otx.core.data.entity.detection import DetBatchPredEntity
 
 
 class TestDFine:
+    @pytest.mark.parametrize(
+        "model",
+        [
+            DFine(label_info=3, model_name="dfine_hgnetv2_n"),
+            DFine(label_info=3, model_name="dfine_hgnetv2_s"),
+            DFine(label_info=3, model_name="dfine_hgnetv2_m"),
+            DFine(label_info=3, model_name="dfine_hgnetv2_l"),
+            DFine(label_info=3, model_name="dfine_hgnetv2_x"),
+        ],
+    )
+    def test_loss(self, model, fxt_data_module):
+        data = next(iter(fxt_data_module.train_dataloader()))
+        data.images = torch.randn([2, 3, 640, 640])
+        model(data)
+
+    @pytest.mark.parametrize(
+        "model",
+        [
+            DFine(label_info=3, model_name="dfine_hgnetv2_n"),
+            DFine(label_info=3, model_name="dfine_hgnetv2_s"),
+            DFine(label_info=3, model_name="dfine_hgnetv2_m"),
+            DFine(label_info=3, model_name="dfine_hgnetv2_l"),
+            DFine(label_info=3, model_name="dfine_hgnetv2_x"),
+        ],
+    )
+    def test_predict(self, model, fxt_data_module):
+        data = next(iter(fxt_data_module.train_dataloader()))
+        data.images = torch.randn(2, 3, 640, 640)
+        model.eval()
+        output = model(data)
+        assert isinstance(output, DetBatchPredEntity)
+
+    @pytest.mark.parametrize(
+        "model",
+        [
+            DFine(label_info=3, model_name="dfine_hgnetv2_n"),
+            DFine(label_info=3, model_name="dfine_hgnetv2_s"),
+            DFine(label_info=3, model_name="dfine_hgnetv2_m"),
+            DFine(label_info=3, model_name="dfine_hgnetv2_l"),
+            DFine(label_info=3, model_name="dfine_hgnetv2_x"),
+        ],
+    )
+    def test_export(self, model):
+        model.eval()
+        output = model.forward_for_tracing(torch.randn(1, 3, 640, 640))
+        assert len(output) == 3
+
     @pytest.fixture()
     def dfine_model(self):
         num_classes = 10
