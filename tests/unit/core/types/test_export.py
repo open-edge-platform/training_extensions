0a69e8ae5f2b4f0f7a79102d94b97f3d6aa5d05c
Vladisalv Sovrasov <sovrasov.vlad@gmail.com>
2024-12-20 17:50:11 +0900

    Update test case implementation

--- tests/unit/core/types/test_export.py
+++ tests/unit/core/types/test_export.py
@@ -1,6 +1,8 @@
 # Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+from copy import deepcopy
+
 import pytest
 from otx.core.config.data import TileConfig
 from otx.core.types.export import TaskLevelExportParameters
@@ -55,13 +57,14 @@ def test_wrap(fxt_label_info, task_type):
     assert ("model_info", "otx_version") in metadata
 
 
-def test_wrap_label_consistency(fxt_label_info):
-    fxt_label_info.label_ids.append("new id")
-    print(fxt_label_info)
+def test_to_metadata_label_consistency(fxt_label_info):
+    label_info = deepcopy(fxt_label_info)
+    label_info.label_ids.append("new id")
+
     params = TaskLevelExportParameters(
         model_type="dummy model",
         task_type="instance_segmentation",
-        label_info=fxt_label_info,
+        label_info=label_info,
         optimization_config={},
     )
 
