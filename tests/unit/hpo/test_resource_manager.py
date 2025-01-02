afec9f1936e62cfee8507ccc16004b074f497e4b
Vladisalv Sovrasov <sovrasov.vlad@gmail.com>
2025-01-03 00:35:09 +0900

    Update licenses

--- tests/unit/hpo/test_resource_manager.py
+++ tests/unit/hpo/test_resource_manager.py
@@ -1,3 +1,6 @@
+# Copyright (C) 2025 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
 import pytest
 from otx.core.types.device import DeviceType
 from otx.hpo import resource_manager as target_file
