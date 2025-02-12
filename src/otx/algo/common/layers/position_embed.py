7e3ea815ceec2a3033c366fbc4f14a9cfa8b23e3
Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
2025-02-12 15:09:58 +0100

    Remove NestedTensor
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>

--- src/otx/algo/common/layers/position_embed.py
+++ src/otx/algo/common/layers/position_embed.py
@@ -8,49 +8,7 @@ from __future__ import annotations
 import math
 
 import torch
-from torch import Tensor, nn
-
-
-class NestedTensor:
-    """Nested tensor class for object detection 3D models."""
-
-    def __init__(self, tensors: Tensor, mask: Tensor) -> None:
-        """Initialize a NestedTensor object.
-
-        Args:
-            tensors (Tensor): The tensors representing the nested structure.
-            mask (Tensor): The mask indicating the valid elements in the tensors.
-        """
-        self.tensors = tensors
-        self.mask = mask
-
-    def to(self, device: torch.device) -> NestedTensor:
-        """Move the NestedTensor object to the specified device.
-
-        Args:
-            device: The device to move the tensors to.
-
-        Returns:
-            NestedTensor: The NestedTensor object with tensors moved to the specified device.
-        """
-        cast_tensor = self.tensors.to(device)
-        cast_mask = self.mask.to(device) if self.mask is not None else None
-        return NestedTensor(cast_tensor, cast_mask)
-
-    def decompose(self) -> tuple[Tensor, Tensor]:
-        """Decompose the NestedTensor object into its constituent tensors and masks."""
-        return self.tensors, self.mask
-
-    def __repr__(self) -> str:
-        """Return a string representation of the NestedTensor object."""
-        return str(self.tensors)
-
-
-def box_cxcylrtb_to_xyxy(x: Tensor) -> Tensor:
-    """Transform bbox from cxcylrtb to xyxy representation."""
-    x_c, y_c, k, r, t, b = x.unbind(-1)
-    bb = [(x_c - k), (y_c - t), (x_c + r), (y_c + b)]
-    return torch.stack(bb, dim=-1)
+from torch import nn
 
 
 class PositionEmbeddingSine(nn.Module):
@@ -82,17 +40,12 @@ class PositionEmbeddingSine(nn.Module):
             scale = 2 * math.pi
         self.scale = scale
 
-    def forward(self, tensor_list: NestedTensor | torch.Tensor) -> torch.Tensor:
+    def forward(self, tensor_list: torch.Tensor) -> torch.Tensor:
         """Forward function for PositionEmbeddingSine module."""
         if isinstance(tensor_list, torch.Tensor):
             x = tensor_list
             mask = torch.zeros((x.size(0), x.size(2), x.size(3)), device=x.device, dtype=torch.bool)
-        elif isinstance(tensor_list, NestedTensor):
-            x = tensor_list.tensors
-            mask = tensor_list.mask
-        else:
-            msg = f"Unrecognized type {type(tensor_list)}"
-            raise TypeError(msg)
+
         not_mask = ~mask
         y_embed = not_mask.cumsum(1)
         x_embed = not_mask.cumsum(2)
