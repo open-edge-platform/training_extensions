2082d7e0e4661b892917b376f06df5eda43f8325
Rajesh Gangireddy <rajesh.gangireddy@intel.com>
2025-04-22 10:36:06 +0200

    Reverts changes in old perf tests

--- tests/perf/test_classification.py
+++ tests/perf/test_classification.py
@@ -39,45 +39,26 @@ class TestPerfSingleLabelClassification(PerfTestBase):
         Benchmark.Model(task="classification/multi_class_cls", name="tv_mobilenet_v3_small", category="other"),
     ]
 
-    DATASET_TEST_CASES = [  # noqa: RUF012
+    DATASET_TEST_CASES = [
         Benchmark.Dataset(
-            name="multiclass_tiny_pneumonia",
-            path=Path("multiclass_classification/mcls_tiny_pneumonia_12_6_200"),
-            group="tiny",
-            num_repeat=5,
-            extra_overrides={},
-        ),
-        Benchmark.Dataset(
-            name="multiclass_tiny_cub_woodpecker",
-            path=Path("multiclass_classification/mcls_tiny_cub_woodpecker_24_12_200"),
-            group="tiny",
-            num_repeat=5,
-            extra_overrides={},
-        ),
-        Benchmark.Dataset(
-            name="multiclass_small_flowers",
-            path=Path("multiclass_classification/mcls_small_flowers_60_12_200"),
+            name=f"multiclass_CUB_small_{idx}",
+            path=Path("multiclass_classification/multiclass_CUB_small") / f"{idx}",
             group="small",
             num_repeat=5,
             extra_overrides={},
-        ),
+        )
+        for idx in (1, 2, 3)
+    ] + [
         Benchmark.Dataset(
-            name="multiclass_small_eurosat",
-            path=Path("multiclass_classification/mcls_small_eurosat_80_40_200"),
-            group="small",
-            num_repeat=5,
-            extra_overrides={},
-        ),
-        Benchmark.Dataset(
-            name="multiclass_medium_resisc",
-            path=Path("multiclass_classification/mcls_medium_resisc_500_100_400"),
+            name="multiclass_CUB_medium",
+            path=Path("multiclass_classification/multiclass_CUB_medium"),
             group="medium",
             num_repeat=5,
             extra_overrides={},
         ),
         Benchmark.Dataset(
-            name="multiclass_large_cub100",
-            path=Path("multiclass_classification/mcls_large_cub100_3764_900_1200"),
+            name="multiclass_food20_large",
+            path=Path("multiclass_classification/multiclass_food20_large"),
             group="large",
             num_repeat=5,
             extra_overrides={},
@@ -137,31 +118,26 @@ class TestPerfMultiLabelClassification(PerfTestBase):
         Benchmark.Model(task="classification/multi_label_cls", name="deit_tiny", category="other"),
     ]
 
-    DATASET_TEST_CASES = [  # noqa: RUF012
+    DATASET_TEST_CASES = [
         Benchmark.Dataset(
-            name="multilabel_tiny_bccd",
-            path=Path("multilabel_classification/mlabel_tiny_bccd_24_6_100"),
-            group="tiny",
-            num_repeat=5,
-            extra_overrides={},
-        ),
-        Benchmark.Dataset(
-            name="multilabel_small_coco",
-            path=Path("multilabel_classification/mlabel_small_coco_80_20_100"),
+            name=f"multilabel_CUB_small_{idx}",
+            path=Path("multilabel_classification/multilabel_CUB_small") / f"{idx}",
             group="small",
             num_repeat=5,
             extra_overrides={},
-        ),
+        )
+        for idx in (1, 2, 3)
+    ] + [
         Benchmark.Dataset(
-            name="multilabel_medium_edsavehicle",
-            path=Path("multilabel_classification/mlabel_medium_edsavehicle_600_150_200"),
+            name="multilabel_CUB_medium",
+            path=Path("multilabel_classification/multilabel_CUB_medium"),
             group="medium",
             num_repeat=5,
             extra_overrides={},
         ),
         Benchmark.Dataset(
-            name="multilabel_large_aid",
-            path=Path("multilabel_classification/mlabel_large_aid_1000_300_300"),
+            name="multilabel_food20_large",
+            path=Path("multilabel_classification/multilabel_food20_large"),
             group="large",
             num_repeat=5,
             extra_overrides={},
@@ -221,31 +197,26 @@ class TestPerfHierarchicalLabelClassification(PerfTestBase):
         Benchmark.Model(task="classification/h_label_cls", name="deit_tiny", category="other"),
     ]
 
-    DATASET_TEST_CASES = [  # noqa: RUF012
+    DATASET_TEST_CASES = [
         Benchmark.Dataset(
-            name="hlabel_tiny_playingcards",
-            path=Path("hlabel_classification/hlabel_tiny_playingcards-2L-6N_36_20_100"),
-            group="tiny",
-            num_repeat=5,
-            extra_overrides={},
-        ),
-        Benchmark.Dataset(
-            name="hlabel_small_cub",
-            path=Path("hlabel_classification/hlabel_small_cub-3L-6N_72_24_100"),
+            name=f"hlabel_CUB_small_{idx}",
+            path=Path("hlabel_classification/hlabel_CUB_small") / f"{idx}",
             group="small",
             num_repeat=5,
             extra_overrides={},
-        ),
+        )
+        for idx in (1, 2, 3)
+    ] + [
         Benchmark.Dataset(
-            name="hlabel_medium_stanfordcars",
-            path=Path("hlabel_classification/hlabel_medium_stanfordcars-26N-3L_350_50_200"),
+            name="hlabel_CUB_medium",
+            path=Path("hlabel_classification/hlabel_CUB_medium"),
             group="medium",
             num_repeat=5,
             extra_overrides={},
         ),
         Benchmark.Dataset(
-            name="hlabel_large_plantdiseases",
-            path=Path("hlabel_classification/hlabel_large_plantdiseases-32N-5L_1000_300_300"),
+            name="cifar100_label_group_datum_format_large",
+            path=Path("hlabel_classification/cifar100_label_group_datum_format_large"),
             group="large",
             num_repeat=5,
             extra_overrides={},
