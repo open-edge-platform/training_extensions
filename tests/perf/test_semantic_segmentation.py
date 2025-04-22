1fd4bb9e9a9789215d87f058e9a158aac52bfad8
Rajesh Gangireddy <rajesh.gangireddy@intel.com>
2025-04-22 10:37:00 +0200

    reverts changes in old perf tests for sem seg

--- tests/perf/test_semantic_segmentation.py
+++ tests/perf/test_semantic_segmentation.py
@@ -26,52 +26,26 @@ class TestPerfSemanticSegmentation(PerfTestBase):
         Benchmark.Model(task="semantic_segmentation", name="dino_v2", category="other"),
     ]
 
-    DATASET_TEST_CASES = [  # noqa: RUF012
+    DATASET_TEST_CASES = [
         Benchmark.Dataset(
-            name="tiny_human_railway_animal",
-            path=Path("semantic_seg/tiny_human_railway_animal_6_6_6"),
-            group="tiny",
-            num_repeat=5,
-            extra_overrides={},
-        ),
-        Benchmark.Dataset(
-            name="tiny_cell_labels",
-            path=Path("semantic_seg/tiny_cell_labels_6_6_6"),
-            group="tiny",
-            num_repeat=5,
-            extra_overrides={},
-        ),
-        Benchmark.Dataset(
-            name="small_satellite_buildings",
-            path=Path("semantic_seg/small_satellite_buildings_20_8_12"),
+            name=f"kvasir_small_{idx}",
+            path=Path("semantic_seg/kvasir_small") / f"{idx}",
             group="small",
             num_repeat=5,
             extra_overrides={},
-        ),
+        )
+        for idx in (1, 2, 3)
+    ] + [
         Benchmark.Dataset(
-            name="small_aerial",
-            path=Path("semantic_seg/small_aerial_50_20_30"),
-            group="small",
-            num_repeat=5,
-            extra_overrides={},
-        ),
-        Benchmark.Dataset(
-            name="medium_kitti",
-            path=Path("semantic_seg/medium_kitti_150_50_50"),
+            name="cityscapes_185_70_medium",
+            path=Path("semantic_seg/cityscapes_185_70_medium"),
             group="medium",
             num_repeat=5,
             extra_overrides={},
         ),
         Benchmark.Dataset(
-            name="medium_voc_otx_cut",
-            path=Path("semantic_seg/medium_voc_otx_cut_662_300_300"),
-            group="medium",
-            num_repeat=5,
-            extra_overrides={},
-        ),
-        Benchmark.Dataset(
-            name="large_idd20k",
-            path=Path("semantic_seg/large_idd20k_lite_1122_204_281"),
+            name="voc_2012_cut_large",
+            path=Path("semantic_seg/voc_2012_cut_large"),
             group="large",
             num_repeat=5,
             extra_overrides={},
