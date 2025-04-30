af35aef1263a78cc5c13261966ca309be7fa7065
Dick Ameln <dick.ameln@intel.com>
2025-04-30 11:00:13 +0200

    add anomaly datasets

--- tests/perf_v2/tasks/anomaly.py
+++ tests/perf_v2/tasks/anomaly.py
@@ -19,20 +19,111 @@ TASK_TYPE = OTXTaskType.ANOMALY
 
 MODEL_TEST_CASES = [
     ModelInfo(task=TASK_TYPE.value, name="padim", category="speed"),
-    ModelInfo(task=TASK_TYPE.value, name="stfpm", category="accuracy"),
+    ModelInfo(task=TASK_TYPE.value, name="uflow", category="accuracy"),
+    ModelInfo(task=TASK_TYPE.value, name="stfpm", category="other"),
 ]
 
 DATASET_TEST_CASES = [
     DatasetInfo(
-        name="mvtec_wood_medium",
-        path=Path("anomaly/mvtec/wood_medium"),
+        name="hazelnut_toy_tiny",
+        path=Path("anomaly/hazelnut_tiny"),
+        group="tiny",
+        extra_overrides={},
+    ),
+    DatasetInfo(
+        name="hazelnut_toy_small",
+        path=Path("anomaly/hazelnut_small"),
+        group="small",
+        extra_overrides={},
+    ),
+    DatasetInfo(
+        name="mvtec_bottle_medium",
+        path=Path("anomaly/mvtec_bottle_medium"),
         group="medium",
         extra_overrides={},
     ),
     DatasetInfo(
-        name="mvtec_hazelnut_large",
-        path=Path("anomaly/mvtec/hazelnut_large"),
-        group="large",
+        name="mvtec_cable_medium",
+        path=Path("anomaly/mvtec_cable_medium"),
+        group="medium",
+        extra_overrides={},
+    ),
+    DatasetInfo(
+        name="mvtec_capsule_medium",
+        path=Path("anomaly/mvtec_capsule_medium"),
+        group="medium",
+        extra_overrides={},
+    ),
+    DatasetInfo(
+        name="mvtec_carpet_medium",
+        path=Path("anomaly/mvtec_carpet_medium"),
+        group="medium",
+        extra_overrides={},
+    ),
+    DatasetInfo(
+        name="mvtec_grid_medium",
+        path=Path("anomaly/mvtec_grid_medium"),
+        group="medium",
+        extra_overrides={},
+    ),
+    DatasetInfo(
+        name="mvtec_hazelnut_medium",
+        path=Path("anomaly/mvtec_hazelnut_medium"),
+        group="medium",
+        extra_overrides={},
+    ),
+    DatasetInfo(
+        name="mvtec_leather_medium",
+        path=Path("anomaly/mvtec_leather_medium"),
+        group="medium",
+        extra_overrides={},
+    ),
+    DatasetInfo(
+        name="mvtec_metal_nut_medium",
+        path=Path("anomaly/mvtec_metal_nut_medium"),
+        group="medium",
+        extra_overrides={},
+    ),
+    DatasetInfo(
+        name="mvtec_pill_medium",
+        path=Path("anomaly/mvtec_pill_medium"),
+        group="medium",
+        extra_overrides={},
+    ),
+    DatasetInfo(
+        name="mvtec_screw_medium",
+        path=Path("anomaly/mvtec_screw_medium"),
+        group="medium",
+        extra_overrides={},
+    ),
+    DatasetInfo(
+        name="mvtec_tile_medium",
+        path=Path("anomaly/mvtec_tile_medium"),
+        group="medium",
+        extra_overrides={},
+    ),
+    DatasetInfo(
+        name="mvtec_toothbrush_medium",
+        path=Path("anomaly/mvtec_toothbrush_medium"),
+        group="medium",
+        extra_overrides={},
+    ),
+    DatasetInfo(
+        name="mvtec_transistor_medium",
+        path=Path("anomaly/mvtec_transistor_medium"),
+        group="medium",
+        extra_overrides={},
+    ),
+    DatasetInfo(
+        name="mvtec_wood_medium",
+        path=Path("anomaly/mvtec_wood_medium"),
+        group="medium",
+        extra_overrides={},
+    ),
+    DatasetInfo(
+        name="mvtec_zipper_medium",
+        path=Path("anomaly/mvtec_zipper_medium"),
+        group="medium",
         extra_overrides={},
     ),
 ]
