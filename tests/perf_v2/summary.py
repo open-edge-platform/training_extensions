4caffb4c95c8b583b58a36aa1d368132ad310e65
Rajesh Gangireddy <rajesh.gangireddy@intel.com>
2025-05-20 09:45:12 +0200

    Update summary.py

--- tests/perf_v2/summary.py
+++ tests/perf_v2/summary.py
@@ -26,7 +26,7 @@ pd.set_option("display.width", None)
 logger = logging.getLogger(__name__)
 
 TASK_METRIC_MAP = {
-    OTXTaskType.ANOMALY: "f1-score",  # perf v2 uses single anomaly task
+    OTXTaskType.ANOMALY: "image_F1Score",  # perf v2 uses single anomaly task
     OTXTaskType.MULTI_CLASS_CLS: "accuracy",
     OTXTaskType.MULTI_LABEL_CLS: "accuracy",
     OTXTaskType.H_LABEL_CLS: "accuracy",
