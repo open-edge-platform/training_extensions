760734d2ad37737d0b7fd7e8837d4f54f0d526c4
Eugene Liu <eugene.liu@intel.com>
2025-06-19 16:13:26 +0100

    Update module docstrings for consistency across various files

--- src/otx/algo/accelerators/xpu.py
+++ src/otx/algo/accelerators/xpu.py
@@ -1,7 +1,7 @@
-"""Lightning accelerator for XPU device."""
 # Copyright (C) 2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-#
+"""Lightning accelerator for XPU device."""
+
 from __future__ import annotations
 
 from typing import Any
