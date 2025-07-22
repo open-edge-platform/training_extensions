54b31b19c84ab8b785660546d50e3b17a8a08dc6
kprokofi <kirill.prokofiev@intel.com>
2025-07-22 23:04:13 +0900

    add hlabel snapshot test

--- tests/unit/backend/native/test_engine.py
+++ tests/unit/backend/native/test_engine.py
@@ -107,6 +107,7 @@ class TestEngine:
         "checkpoint",
         [
             "tests/assets/test_snapshots/dummy_checkpoint_cls_2.2.0.ckpt",
+            "tests/assets/test_snapshots/dummy_checkpoint_hlabel_2.2.0.ckpt",
             "tests/assets/test_snapshots/dummy_checkpoint_is_2.2.0.ckpt",
             "tests/assets/test_snapshots/dummy_checkpoint_det_2.2.0.ckpt",
             "tests/assets/test_snapshots/dummy_checkpoint_anomaly_2.2.0.ckpt",
