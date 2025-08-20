37109c38d802b63adf6942b00d4a8d4dbc3c957d
kprokofi <kirill.prokofiev@intel.com>
2025-08-20 22:30:57 +0900

    fix finding task type in ir

--- src/otx/backend/openvino/engine.py
+++ src/otx/backend/openvino/engine.py
@@ -95,9 +95,7 @@ class OVEngine(Engine):
             "detection": OTXTaskType.DETECTION,
             "instance_segmentation": OTXTaskType.INSTANCE_SEGMENTATION,
             "keypoint_detection": OTXTaskType.KEYPOINT_DETECTION,
-            "anomaly_classification": OTXTaskType.ANOMALY_CLASSIFICATION,
-            "anomaly_detection": OTXTaskType.ANOMALY_DETECTION,
-            "anomaly_segmentation": OTXTaskType.ANOMALY_SEGMENTATION,
+            "anomaly": OTXTaskType.ANOMALY,
         }
 
         tree = Elet.parse(ir_xml)
@@ -109,13 +107,21 @@ class OVEngine(Engine):
 
         task_type = rt_info.find(".//task_type")
         if task_type is None:
-            msg = (
-                "No <task_type> found in the IR model XML file. Please check the model file."
-                "Task cannot be derived from the model."
-            )
-            raise ValueError(msg)
+            # check old Anomaly models
+            model_info = rt_info.find(".//model_info")
+            if model_info:
+                model_type = model_info.find(".//model_type")
+                if model_type is not None and "anomaly" in model_type.attrib.get("value", "").lower():
+                    task_type = "anomaly"
+            if task_type is None:
+                msg = (
+                    "No <task_type> found in the IR model XML file. Please check the model file."
+                    "Task cannot be derived from the model."
+                )
+                raise ValueError(msg)
+        else:
+            task_type = task_type.attrib.get("value")
 
-        task_type = task_type.attrib.get("value")
         if task_type == "classification":
             if rt_info.find(".//hierarchical").attrib.get("value") == "True":
                 otx_task_name = task_type + "_hcl"
@@ -123,9 +129,6 @@ class OVEngine(Engine):
                 otx_task_name = task_type + "_mlc"
             else:
                 otx_task_name = task_type + "_mc"
-        elif task_type == "anomaly":
-            sub_type = rt_info.find(".//task").attrib.get("value")
-            otx_task_name = task_type + f"_{sub_type}"
         else:
             otx_task_name = task_type
 
