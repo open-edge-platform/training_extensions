bf55bfff5951c1b68f7a70ad81e7525b6b580cbe
Colorado, Camilo <camilo.colorado@intel.com>
2025-08-22 16:30:47 +0200

    spectrum vars

--- ui/src/features/pipelines/modal/new-project-link.component.tsx
+++ ui/src/features/pipelines/modal/new-project-link.component.tsx
@@ -1,3 +1,6 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
 import { Text } from '@geti/ui';
 import { AddCircle } from '@geti/ui/icons';
 import { Link } from 'react-router-dom';
@@ -8,9 +11,9 @@ import classes from './new-project-link.module.scss';
 
 export const NewProjectLink = () => {
     return (
-        <Link to={paths.pipeline.model.pattern} className={classes.link}>
+        <Link to={paths.pipeline.new.pattern} className={classes.link}>
             <AddCircle />
-            <Text>Add another project 2</Text>
+            <Text>Add another project</Text>
         </Link>
     );
 };
