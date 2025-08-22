b34b9a7210766b0bb92d53f895195edb6f42c137
Joao Vilaca <joao.vilaca@intel.com>
2025-08-22 16:45:32 +0200

    Add edit bounding box tool & friends

---
+++ ui/src/components/annotation/shape-factory.component.tsx
@@ -0,0 +1,20 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import { Shape } from '../shapes/interfaces';
+import { Polygon } from '../shapes/polygon.component';
+import { Rectangle } from '../shapes/rectangle.component';
+
+interface ShapeFactoryProps {
+    shape: Shape;
+    styles: React.SVGProps<SVGPolygonElement & SVGRectElement>;
+    ariaLabel: string;
+}
+export const ShapeFactory = ({ shape, styles, ariaLabel }: ShapeFactoryProps) => {
+    switch (shape.shapeType) {
+        case 'rect':
+            return <Rectangle rect={shape} styles={styles} ariaLabel={ariaLabel} />;
+        case 'polygon':
+            return <Polygon polygon={shape} styles={styles} ariaLabel={ariaLabel} />;
+    }
+};
---
+++ ui/src/components/bounding-box-tool/translate-shape.component.tsx
@@ -0,0 +1,81 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import { PointerEvent, useState } from 'react';
+
+import { Annotation } from '../annotation/annotation.component';
+import { Annotation as AnnotationInterface, Point } from '../shapes/interfaces';
+import { allowPanning, isLeftButton } from './utils';
+
+const STROKE_WIDTH = 2;
+
+interface TranslateShapeProps {
+    zoom: number;
+    annotation: AnnotationInterface;
+    translateShape: ({ x, y }: { x: number; y: number }) => void;
+    onComplete: () => void;
+}
+
+export const TranslateShape = ({ zoom, annotation, onComplete, translateShape }: TranslateShapeProps): JSX.Element => {
+    const [dragFromPoint, setDragFromPoint] = useState<null | Point>(null);
+
+    const onPointerDown = (event: PointerEvent<SVGSVGElement>): void => {
+        if (dragFromPoint !== null) {
+            return;
+        }
+
+        if (event.pointerType === 'touch' || !isLeftButton(event)) {
+            return;
+        }
+
+        const mouse = { x: Math.round(event.clientX / zoom), y: Math.round(event.clientY / zoom) };
+
+        event.currentTarget.setPointerCapture(event.pointerId);
+
+        setDragFromPoint(mouse);
+    };
+
+    const onPointerMove = (event: PointerEvent<SVGSVGElement>) => {
+        event.preventDefault();
+
+        if (dragFromPoint === null) {
+            return;
+        }
+
+        const mouse = { x: Math.round(event.clientX / zoom), y: Math.round(event.clientY / zoom) };
+
+        translateShape({
+            x: mouse.x - dragFromPoint.x,
+            y: mouse.y - dragFromPoint.y,
+        });
+
+        setDragFromPoint(mouse);
+    };
+
+    const onPointerUp = (event: PointerEvent<SVGSVGElement>) => {
+        if (dragFromPoint === null) {
+            return;
+        }
+
+        event.preventDefault();
+        setDragFromPoint(null);
+        event.currentTarget.releasePointerCapture(event.pointerId);
+        onComplete();
+    };
+
+    return (
+        <g
+            id={`translate-annotation-${annotation.id}`}
+            stroke='var(--energy-blue)'
+            strokeWidth={STROKE_WIDTH / zoom}
+            aria-label='Drag to move shape'
+            onPointerDown={allowPanning(onPointerDown)}
+            onPointerMove={onPointerMove}
+            onPointerUp={onPointerUp}
+            onPointerLeave={onPointerUp}
+            style={{ pointerEvents: 'auto', cursor: 'move' }}
+        >
+            <Annotation annotation={annotation} />
+        </g>
+    );
+};
