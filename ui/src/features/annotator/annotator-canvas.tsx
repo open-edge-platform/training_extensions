226bfe54837fc5cc92df32710bee620e74f221f5
Joao Vilaca <joao.vilaca@intel.com>
2025-09-02 11:37:28 +0200

    Readd zoomprovider

--- ui/src/features/annotator/annotator-canvas.tsx
+++ ui/src/features/annotator/annotator-canvas.tsx
@@ -5,6 +5,7 @@ import { Grid, View } from '@geti/ui';
 import { isEmpty } from 'lodash-es';
 
 import thumbnailUrl from '../../assets/mocked-project-thumbnail.png';
+import { ZoomProvider } from '../../components/zoom/zoom';
 import { ZoomTransform } from '../../components/zoom/zoom-transform';
 import { response } from '../dataset/mock-response';
 import { Annotations } from './annotations';
@@ -15,23 +16,25 @@ export const AnnotatorCanvas = ({ mediaItem, isFocussed }: { mediaItem: Item; is
     const size = { width: mediaItem.width, height: mediaItem.height };
 
     return (
-        <ZoomTransform target={size}>
-            <Grid areas={['innercanvas']} width={'100%'} height='100%'>
-                <View gridArea={'innercanvas'}>
-                    <img src={thumbnailUrl} alt='Collected data' />
-                </View>
-
-                {!isEmpty(mediaItem.annotations) && (
+        <ZoomProvider>
+            <ZoomTransform target={size}>
+                <Grid areas={['innercanvas']} width={'100%'} height='100%'>
                     <View gridArea={'innercanvas'}>
-                        <Annotations
-                            annotations={mediaItem.annotations}
-                            width={size.width}
-                            height={size.height}
-                            isFocussed={isFocussed}
-                        />
+                        <img src={thumbnailUrl} alt='Collected data' />
                     </View>
-                )}
-            </Grid>
-        </ZoomTransform>
+
+                    {!isEmpty(mediaItem.annotations) && (
+                        <View gridArea={'innercanvas'}>
+                            <Annotations
+                                annotations={mediaItem.annotations}
+                                width={size.width}
+                                height={size.height}
+                                isFocussed={isFocussed}
+                            />
+                        </View>
+                    )}
+                </Grid>
+            </ZoomTransform>
+        </ZoomProvider>
     );
 };
