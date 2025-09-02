ae363a96ba9325f345540d0bd61915f2a5660ac6
Dawid Wesołowski <dawid.wesolowski@intel.com>
2025-09-02 10:54:20 +0200

    refactor: Remove JSX.Element and use ReactNode when necessary

--- ui/src/components/bounding-box-tool/anchor.component.tsx
+++ ui/src/components/bounding-box-tool/anchor.component.tsx
@@ -34,7 +34,7 @@ export const Anchor = ({
     onStart,
     moveAnchorTo,
     onComplete,
-}: AnchorProps): JSX.Element => {
+}: AnchorProps) => {
     const [dragFrom, setDragFrom] = useState<Point | null>(null);
 
     const onPointerDown = (event: PointerEvent) => {
