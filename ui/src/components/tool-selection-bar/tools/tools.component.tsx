d0ebdd680a1b9c2471620720668a8bcaeec46439
Joao Vilaca <joao.vilaca@intel.com>
2025-09-05 15:40:20 +0200

    Move interface; Update tools component

--- ui/src/components/tool-selection-bar/tools/tools.component.tsx
+++ ui/src/components/tool-selection-bar/tools/tools.component.tsx
@@ -1,35 +1,25 @@
 // Copyright (C) 2025 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-import { useState } from 'react';
-
 import { Divider } from '@geti/ui';
-import { Polygon, SegmentAnythingIcon, Selector } from '@geti/ui/icons';
+import { BoundingBox, Selector } from '@geti/ui/icons';
 
+import { useAnnotator } from '../../../features/annotator/annotator-provider.component';
 import { IconWrapper } from '../icon-wrapper.component';
 
 export const Tools = () => {
-    type ToolType = 'selection' | 'bounding-box' | 'polygon' | 'sam';
-    const [activeTool, setActiveTool] = useState<ToolType>('selection');
-
-    const handleSelectTool = (tool: ToolType) => {
-        setActiveTool(tool);
-    };
+    const { activeTool, selectTool } = useAnnotator();
 
     return (
         <>
-            <IconWrapper onPress={() => handleSelectTool('selection')} isSelected={activeTool === 'selection'}>
+            <IconWrapper onPress={() => selectTool('selection')} isSelected={activeTool === 'selection'}>
                 <Selector data-tool='selection' />
             </IconWrapper>
 
             <Divider size='S' />
 
-            <IconWrapper onPress={() => handleSelectTool('polygon')} isSelected={activeTool === 'polygon'}>
-                <Polygon data-tool='polygon' />
-            </IconWrapper>
-
-            <IconWrapper onPress={() => handleSelectTool('sam')} isSelected={activeTool === 'sam'}>
-                <SegmentAnythingIcon data-tool='sam' />
+            <IconWrapper onPress={() => selectTool('bounding-box')} isSelected={activeTool === 'bounding-box'}>
+                <BoundingBox />
             </IconWrapper>
         </>
     );
