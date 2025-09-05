c7f167ce2620140795d296dc4c50601e9d974559
Vitalii Karniushin <vitalii.karniushin@intel.com>
2025-09-05 09:59:39 +0200

    test: fix unit tests for PipelineService

--- backend/tests/unit/services/test_pipeline_service.py
+++ backend/tests/unit/services/test_pipeline_service.py
@@ -1,22 +1,11 @@
 # Copyright (C) 2025 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from multiprocessing.shared_memory import SharedMemory
 from unittest.mock import MagicMock, patch
 
 import pytest
 
-from app.schemas import PipelineStatus
 from app.services import MetricsService, PipelineService
-from app.services.metrics_service import SHM_NAME, SIZE
-
-
-@pytest.fixture(scope="module", autouse=True)
-def fxt_metrics_mem():
-    shm = SharedMemory(name=SHM_NAME, create=True, size=SIZE)
-    yield
-    shm.close()
-    shm.unlink()
 
 
 @pytest.fixture
@@ -42,7 +31,7 @@ class TestPipelineServiceUnit:
             patch("app.services.pipeline_service.PipelineService.get_pipeline_by_id") as mock_get_pipeline_by_id,
         ):
             mock_get_pipeline_by_id.return_value = fxt_running_pipeline
-            metrics = fxt_pipeline_service.get_pipeline_metrics(fxt_running_pipeline.id, time_window=60)
+            metrics = fxt_pipeline_service.get_pipeline_metrics(fxt_running_pipeline.project_id, time_window=60)
 
         fxt_metrics_service.get_latency_measurements.assert_called_once_with(model_id=fxt_model.id, time_window=60)
         assert metrics.time_window.time_window == 60
@@ -61,7 +50,7 @@ class TestPipelineServiceUnit:
             patch("app.services.pipeline_service.PipelineService.get_pipeline_by_id") as mock_get_pipeline_by_id,
         ):
             mock_get_pipeline_by_id.return_value = fxt_running_pipeline
-            metrics = fxt_pipeline_service.get_pipeline_metrics(fxt_running_pipeline.id, time_window=60)
+            metrics = fxt_pipeline_service.get_pipeline_metrics(fxt_running_pipeline.project_id, time_window=60)
 
         fxt_metrics_service.get_latency_measurements.assert_called_once_with(model_id=fxt_model.id, time_window=60)
         assert metrics.time_window.time_window == 60
@@ -71,11 +60,11 @@ class TestPipelineServiceUnit:
         assert metrics.inference.latency.p95_ms is None
         assert metrics.inference.latency.latest_ms is None
 
-    def test_get_pipeline_metrics_not_running(self, fxt_pipeline_service, fxt_pipeline):
+    def test_get_pipeline_metrics_not_running(self, fxt_pipeline_service, fxt_default_pipeline):
         """Test retrieving metrics for pipeline that is not running raises error."""
         with (
             patch("app.services.pipeline_service.PipelineService.get_pipeline_by_id") as mock_get_pipeline_by_id,
             pytest.raises(ValueError, match="Cannot get metrics for a pipeline that is not running."),
         ):
-            mock_get_pipeline_by_id.return_value = fxt_pipeline.model_copy(update={"status": PipelineStatus.IDLE})
-            fxt_pipeline_service.get_pipeline_metrics(fxt_pipeline.id)
+            mock_get_pipeline_by_id.return_value = fxt_default_pipeline
+            fxt_pipeline_service.get_pipeline_metrics(fxt_default_pipeline.project_id)
