9f05a8487da813fb09b9c80a9e6363238db07fa8
Joao Vilaca <joao.vilaca@intel.com>
2025-09-05 15:41:10 +0200

    Add ToolManager & useActiveTool hook

---
+++ ui/src/features/annotator/hooks/use-active-tool.hook.ts
@@ -0,0 +1,16 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import { useState } from 'react';
+
+import { ToolType } from '../../../components/tool-selection-bar/tools/interface';
+
+export const useActiveTool = () => {
+    const [activeTool, setActiveTool] = useState<ToolType>('bounding-box');
+
+    const handleSelectTool = (tool: ToolType) => {
+        setActiveTool(tool);
+    };
+
+    return { activeTool, selectTool: handleSelectTool };
+};
---
+++ ui/src/features/annotator/tool-manager.component.tsx
@@ -0,0 +1,38 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import { useZoom } from '../../components/zoom/zoom';
+import { useAnnotator } from './annotator-provider.component';
+import { EditBoundingBox } from './tools/bounding-box-tool/bounding-box-tool.component';
+import { SelectionTool } from './tools/selection-tool/selection-tool.component';
+import { Annotation, Rect } from './types';
+
+type ToolManager = {
+    width: number;
+    height: number;
+};
+export const ToolManager = ({ width, height }: ToolManager) => {
+    const zoom = useZoom();
+    const { selectedAnnotation, updateAnnotation, activeTool } = useAnnotator();
+
+    if (!selectedAnnotation) {
+        return null;
+    }
+
+    if (activeTool === 'bounding-box') {
+        return (
+            <EditBoundingBox
+                annotation={selectedAnnotation as Annotation & { shape: Rect }}
+                roi={{ x: 0, y: 0, width, height }}
+                zoom={zoom.scale}
+                updateAnnotation={updateAnnotation}
+            />
+        );
+    }
+
+    if (activeTool === 'selection') {
+        return <SelectionTool annotation={selectedAnnotation} updateAnnotation={updateAnnotation} />;
+    }
+
+    return null;
+};
