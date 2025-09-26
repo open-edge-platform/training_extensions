9d049a560139527886bc9f1544413fb6dff8a7a6
Colorado, Camilo <camilo.colorado@intel.com>
2025-09-26 13:22:30 +0200

    add mqtt option

--- application/ui/src/features/inference/sinks/sink-options.tsx
+++ application/ui/src/features/inference/sinks/sink-options.tsx
@@ -3,14 +3,18 @@
 
 import { useState } from 'react';
 
-import { ReactComponent as Image } from '../../../assets/icons/images-folder.svg';
+import { ReactComponent as FolderIcon } from '../../../assets/icons/folder.svg';
+import { ReactComponent as MqttIcon } from '../../../assets/icons/mqtt.svg';
+import { ReactComponent as WebhookIcon } from '../../../assets/icons/webhook.svg';
 import { DisclosureGroup } from '../sources/disclosure-group.component';
 import { Folder } from './folder.component';
+import { Mqtt } from './mqtt.component';
 import { Webhook } from './webhook.component';
 
 const inputs = [
-    { label: 'Folder', value: 'folder', content: <Folder />, icon: <Image width={'24px'} /> },
-    { label: 'Webhook', value: 'webhook', content: <Webhook />, icon: <Image width={'24px'} /> },
+    { label: 'Folder', value: 'folder', content: <Folder />, icon: <FolderIcon width={'24px'} /> },
+    { label: 'Webhook', value: 'webhook', content: <Webhook />, icon: <WebhookIcon width={'24px'} /> },
+    { label: 'MQTT', value: 'mqtt', content: <Mqtt />, icon: <MqttIcon width={'24px'} /> },
 ];
 
 export const SinkOptions = () => {
