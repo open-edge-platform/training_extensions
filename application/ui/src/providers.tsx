062e91cbcc842d1ab8894809e21a5208c2bf7408
Joao Vilaca <joao.vilaca@intel.com>
2025-09-29 10:26:02 +0200

    Update render method

--- application/ui/src/providers.tsx
+++ application/ui/src/providers.tsx
@@ -1,17 +1,14 @@
 // Copyright (C) 2025 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-import { ReactNode } from 'react';
-
 import { Toast } from '@geti/ui';
 import { ThemeProvider } from '@geti/ui/theme';
 import { MutationCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { MemoryRouterProps, RouterProvider } from 'react-router';
-import { MemoryRouter as Router } from 'react-router-dom';
+import { RouterProvider } from 'react-router';
 
 import { router } from './router';
 
-const queryClient = new QueryClient({
+export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
             gcTime: 30 * 60 * 1000,
@@ -30,17 +27,6 @@ export const Providers = () => {
         <QueryClientProvider client={queryClient}>
             <ThemeProvider router={router}>
                 <RouterProvider router={router} />
-                <Toast />
-            </ThemeProvider>
-        </QueryClientProvider>
-    );
-};
-
-export const TestProviders = ({ children, routerProps }: { children: ReactNode; routerProps?: MemoryRouterProps }) => {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <ThemeProvider>
-                <Router {...routerProps}>{children}</Router>
                 <Toast />
             </ThemeProvider>
         </QueryClientProvider>
