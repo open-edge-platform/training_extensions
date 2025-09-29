8feb8cde83e356c8d848c636207bbce2bc1bb21b
Joao Vilaca <joao.vilaca@intel.com>
2025-09-29 10:26:12 +0200

    Add unit test for addmediabutton

---
+++ application/ui/src/components/add-media-button/add-media-button.component.test.tsx
@@ -0,0 +1,23 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import { fireEvent, render, screen } from '@test-utils/render';
+
+import { AddMediaButton } from './add-media-button.component';
+
+describe('AddMediaButton', () => {
+    it('calls onFilesSelected correctly', () => {
+        const mockOnFilesSelected = vi.fn();
+        const mockFile = new File(['file content'], 'test-image.jpg', {
+            type: 'image/jpeg',
+            lastModified: Date.now(),
+        });
+
+        render(<AddMediaButton onFilesSelected={mockOnFilesSelected} />);
+
+        const input = screen.getByLabelText(/Upload media files/);
+        fireEvent.change(input, { target: { files: [mockFile] } });
+
+        expect(mockOnFilesSelected).toHaveBeenCalledWith([mockFile]);
+    });
+});
