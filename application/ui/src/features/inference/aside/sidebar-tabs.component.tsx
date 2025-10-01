0e3aba9fc43fe3663442e18419da723982e621b7
João Vilaça <joao.vilaca@intel.com>
2025-10-01 14:20:28 +0200

    Data collection settings (#4809)

---
+++ application/ui/src/features/inference/aside/sidebar-tabs.component.tsx
@@ -0,0 +1,70 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import { useState } from 'react';
+
+import { Flex, Grid, ToggleButton, View } from '@geti/ui';
+import { Gear, GraphChart } from '@geti/ui/icons';
+
+import { DataCollection } from './data-collection.component';
+import { Graphs } from './graphs.component';
+
+import styles from './sidebar-tabs.module.scss';
+
+const TABS = [
+    { label: 'Data collection policy', icon: <Gear />, content: <DataCollection /> },
+    { label: 'Model statistics', icon: <GraphChart />, content: <Graphs /> },
+];
+
+interface TabProps {
+    tabs: (typeof TABS)[number][];
+    selectedTab: string;
+}
+
+const SidebarTabs = ({ tabs, selectedTab }: TabProps) => {
+    const [tab, setTab] = useState<string | null>(selectedTab);
+
+    const gridTemplateColumns = tab !== null ? ['clamp(size-4600, 35vw, 40rem)', 'size-600'] : ['0px', 'size-600'];
+
+    const content = tabs.find(({ label }) => label === tab)?.content;
+
+    return (
+        <Grid
+            gridArea={'aside'}
+            UNSAFE_className={styles.container}
+            columns={gridTemplateColumns}
+            data-expanded={tab !== null}
+            minHeight={0}
+        >
+            <View
+                gridColumn={'1/2'}
+                UNSAFE_className={styles.sidebarContent}
+                backgroundColor={'gray-100'}
+                paddingY={'size-200'}
+                paddingX={'size-300'}
+            >
+                {content}
+            </View>
+            <View gridColumn={'2/3'} backgroundColor={'gray-200'} padding={'size-100'}>
+                <Flex direction={'column'} height={'100%'} alignItems={'center'} gap={'size-100'}>
+                    {tabs.map(({ label, icon }) => (
+                        <ToggleButton
+                            key={label}
+                            isQuiet
+                            isSelected={label === tab}
+                            onChange={() => setTab(label === tab ? null : label)}
+                            UNSAFE_className={styles.toggleButton}
+                            aria-label={`Toggle ${label} tab`}
+                        >
+                            {icon}
+                        </ToggleButton>
+                    ))}
+                </Flex>
+            </View>
+        </Grid>
+    );
+};
+
+export const Sidebar = () => {
+    return <SidebarTabs tabs={TABS} selectedTab={TABS[0].label} />;
+};
