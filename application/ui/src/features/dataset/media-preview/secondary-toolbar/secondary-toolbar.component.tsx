ae1ef2d71d292dfb41af0b111152953ad1a36f26
Joao Vilaca <joao.vilaca@intel.com>
2025-10-13 13:13:23 +0200

    Add initial label selection UI

--- application/ui/src/features/dataset/media-preview/secondary-toolbar/secondary-toolbar.component.tsx
+++ application/ui/src/features/dataset/media-preview/secondary-toolbar/secondary-toolbar.component.tsx
@@ -1,14 +1,16 @@
 // Copyright (C) 2025 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
+import { ReactNode } from 'react';
+
 import { Flex, Grid } from '@geti/ui';
 
-export const SecondaryToolbar = () => {
+export const SecondaryToolbar = ({ content }: { content: ReactNode }) => {
     return (
         <Flex height={'100%'} alignItems={'center'} justifyContent={'center'} margin={'size-100'}>
             <Grid>
                 <Flex>Something</Flex>
-                <Flex>Something else</Flex>
+                <Flex>{content}</Flex>
             </Grid>
         </Flex>
     );
