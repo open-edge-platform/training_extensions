7c2c77d4b85370e72e66de463ec8ca6dfa6ebcea
Joao Vilaca <joao.vilaca@intel.com>
2025-10-13 13:13:23 +0200

    Add initial label selection UI

--- application/ui/src/features/dataset/media-preview/secondary-toolbar/secondary-toolbar.component.tsx
+++ application/ui/src/features/dataset/media-preview/secondary-toolbar/secondary-toolbar.component.tsx
@@ -1,25 +1,16 @@
 // Copyright (C) 2025 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
+import { ReactNode } from 'react';
+
 import { Flex, Grid } from '@geti/ui';
 
-import { LabelPicker } from './label-picker.component';
-import { useSecondaryToolbarState } from './use-secondary-toolbar-state.hook';
-
-import classes from '../media-preview.module.scss';
-
-export const SecondaryToolbar = () => {
-    const { isHidden, projectLabels, toggleLabel, annotationsToUpdate } = useSecondaryToolbarState();
-
-    const annotationLabelId = annotationsToUpdate.at(0)?.labels?.at(0)?.id;
-    const selectedLabel = projectLabels.find((label) => label.id === annotationLabelId) ?? null;
-
+export const SecondaryToolbar = ({ content }: { content: ReactNode }) => {
     return (
-        <Flex height={'100%'} alignItems={'center'} margin={'size-100'} minHeight={'size-1200'}>
-            <Grid UNSAFE_className={classes.toolbarGrid} isHidden={isHidden}>
-                <Flex UNSAFE_className={classes.toolbarSection}>
-                    <LabelPicker selectedLabel={selectedLabel} labels={projectLabels} onSelect={toggleLabel} />
-                </Flex>
+        <Flex height={'100%'} alignItems={'center'} justifyContent={'center'} margin={'size-100'}>
+            <Grid>
+                <Flex>Something</Flex>
+                <Flex>{content}</Flex>
             </Grid>
         </Flex>
     );
