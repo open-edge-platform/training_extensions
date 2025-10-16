eae0dd719f03824fcfedda9bb869f8bcd29a4136
Joao Vilaca <joao.vilaca@intel.com>
2025-10-16 15:20:22 +0200

    Reuse annotationshape inside annotationshapewithlabels

--- application/ui/src/features/annotator/annotations/annotation-shape-with-labels.component.tsx
+++ application/ui/src/features/annotator/annotations/annotation-shape-with-labels.component.tsx
@@ -6,37 +6,35 @@ import { useSecondaryToolbarState } from 'src/features/dataset/media-preview/sec
 
 import { Annotation, Polygon } from '../types';
 import { AnnotationLabels } from './annotation-labels.component';
-import { getFormattedPoints } from './utils';
+import { AnnotationShape } from './annotation-shape.component';
 
 type AnnotationShapeProps = {
     annotation: Annotation;
 };
+
 export const AnnotationShapeWithLabels = ({ annotation }: AnnotationShapeProps) => {
     const { shape, labels } = annotation;
-    const color = labels.length ? labels[0].color : '--annotation-fill';
     const { removeLabels } = useSecondaryToolbarState();
 
     if (shape.type === 'rectangle') {
         return (
             <g transform={`translate(${shape.x}, ${shape.y})`}>
-                <rect aria-label='annotation rect' x={0} y={0} width={shape.width} height={shape.height} fill={color} />
+                <AnnotationShape annotation={{ ...annotation, shape: { ...shape, x: 0, y: 0 } }} />
                 <AnnotationLabels labels={labels} onRemove={removeLabels} />
             </g>
         );
     }
 
+    // For polygon, use polylabel to find the optimal label position
     const polygonPoints = (shape as Polygon).points;
     const polygonCoords = [polygonPoints.map((point) => [point.x, point.y])];
-    const [labelX, labelY] = polylabel(polygonCoords);
+    const [labelX, labelY] = polylabel(polygonCoords, 1.0);
 
     return (
         <g transform={`translate(${labelX}, ${labelY})`}>
-            <polygon
-                aria-label='annotation polygon'
-                points={getFormattedPoints(polygonPoints)}
-                fill={color}
-                transform={`translate(${-labelX}, ${-labelY})`}
-            />
+            <g transform={`translate(${-labelX}, ${-labelY})`}>
+                <AnnotationShape annotation={annotation} />
+            </g>
             <AnnotationLabels labels={labels} onRemove={removeLabels} />
         </g>
     );
