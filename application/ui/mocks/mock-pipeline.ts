6d4a8f87ec3e56546c292d46928df70e9cf1b6eb
Joao Vilaca <joao.vilaca@intel.com>
2025-10-16 09:44:29 +0200

    Update component tests

---
+++ application/ui/mocks/mock-pipeline.ts
@@ -0,0 +1,39 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import { SchemaPipelineView } from './../src/api/openapi-spec.d';
+
+export const getMockedPipeline = (customPipeline?: Partial<SchemaPipelineView>): SchemaPipelineView => {
+    return {
+        project_id: '123',
+        status: 'running' as const,
+        data_collection_policies: [],
+        source: {
+            id: 'source-id',
+            name: 'source',
+            source_type: 'video_file' as const,
+            video_path: 'video.mp4',
+        },
+        model: {
+            id: '1',
+            architecture: 'Object_Detection_TestModel',
+            training_info: {
+                status: 'successful' as const,
+                label_schema_revision: {},
+                configuration: {},
+            },
+            files_deleted: false,
+        },
+        sink: {
+            id: 'sink-id',
+            name: 'sink',
+            folder_path: 'data/sink',
+            output_formats: ['image_original', 'image_with_predictions', 'predictions'] as Array<
+                'image_original' | 'image_with_predictions' | 'predictions'
+            >,
+            rate_limit: 0.2,
+            sink_type: 'folder' as const,
+        },
+        ...customPipeline,
+    };
+};
