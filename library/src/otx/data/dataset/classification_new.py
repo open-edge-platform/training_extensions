64e58ca09976eed03db36e47adadfdc3d96f6089
Albert van Houten <albert.van.houten@intel.com>
2025-10-20 09:59:34 +0200

    Use new datumaro for multi label and hierarchical classification (#4815)
    Signed-off-by: Albert van Houten <albert.van.houten@intel.com>

--- library/src/otx/data/dataset/classification_new.py
+++ library/src/otx/data/dataset/classification_new.py
@@ -7,9 +7,18 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from otx import LabelInfo
+import torch
+from datumaro import Label
+from torch.nn import functional
+from torchvision.transforms.v2.functional import to_dtype, to_image
+
+from otx import HLabelInfo, LabelInfo
 from otx.data.dataset.base_new import OTXDataset
-from otx.data.entity.sample import ClassificationSample
+from otx.data.entity.sample import (
+    ClassificationHierarchicalSample,
+    ClassificationMultiLabelSample,
+    ClassificationSample,
+)
 
 if TYPE_CHECKING:
     from datumaro.experimental import Dataset
@@ -51,4 +60,152 @@ class OTXMulticlassClsDataset(OTXDataset):
             if label_id not in idx_list_per_classes:
                 idx_list_per_classes[label_id] = []
             idx_list_per_classes[label_id].append(idx)
+        return idx_list_per_classes
+
+
+class OTXMultilabelClsDataset(OTXDataset):
+    """OTXDataset class for multi-label classification task."""
+
+    def __init__(self, dm_subset: Dataset, **kwargs) -> None:
+        sample_type = ClassificationMultiLabelSample
+        dm_subset = dm_subset.convert_to_schema(sample_type)
+        super().__init__(dm_subset=dm_subset, sample_type=sample_type, **kwargs)
+
+        labels = dm_subset.schema.attributes["label"].categories.labels
+        self.label_info = LabelInfo(
+            label_names=labels,
+            label_groups=[labels],
+            label_ids=[str(i) for i in range(len(labels))],
+        )
+        self.num_classes = len(labels)
+
+    def _get_item_impl(self, index: int) -> ClassificationMultiLabelSample | None:
+        item = self.dm_subset[index]
+        item.image = to_dtype(to_image(item.image), dtype=torch.float32)
+        item.label = self._convert_to_onehot(torch.as_tensor(list(item.label)), ignored_labels=[])
+        return self._apply_transforms(item)
+
+    def _convert_to_onehot(self, labels: torch.tensor, ignored_labels: list[int]) -> torch.tensor:
+        """Convert label to one-hot vector format."""
+        # Torch's one_hot() expects the input to be of type long
+        # However, when labels are empty, they are of type float32
+        onehot = functional.one_hot(labels.long(), self.num_classes).sum(0).clamp_max_(1)
+        if ignored_labels:
+            for ignore_label in ignored_labels:
+                onehot[ignore_label] = -1
+        return onehot
+
+    def get_idx_list_per_classes(self, use_string_label: bool = False) -> dict[int, list[int]]:
+        """Get a dictionary mapping class labels (string or int) to lists of samples.
+
+        Args:
+            use_string_label (bool): If True, use string class labels as keys.
+                If False, use integer indices as keys.
+        """
+        idx_list_per_classes: dict[int, list[int]] = {}
+        for idx in range(len(self)):
+            item = self.dm_subset[idx]
+            labels = item.label.tolist()
+            if use_string_label:
+                labels = [self.label_info.label_names[label] for label in labels]
+            for label in labels:
+                if label not in idx_list_per_classes:
+                    idx_list_per_classes[label] = []
+                idx_list_per_classes[label].append(idx)
+        return idx_list_per_classes
+
+
+class OTXHlabelClsDataset(OTXDataset):
+    """OTXDataset class for H-label classification task."""
+
+    def __init__(self, dm_subset: Dataset, **kwargs) -> None:
+        sample_type = ClassificationHierarchicalSample
+        dm_subset = dm_subset.convert_to_schema(sample_type)
+        super().__init__(dm_subset=dm_subset, sample_type=sample_type, **kwargs)
+        if self.data_format != "arrow":
+            raise ValueError("The data format should be arrow.")  # noqa: EM101, TRY003
+        self.dm_categories = self.dm_subset.schema.attributes["label"].categories
+        self.label_info = HLabelInfo.from_dm_label_groups_arrow(self.dm_categories)
+
+        self.id_to_name_mapping = dict(zip(self.label_info.label_ids, self.label_info.label_names))
+        self.id_to_name_mapping[""] = ""
+
+        if self.label_info.num_multiclass_heads == 0:
+            msg = "The number of multiclass heads should be larger than 0."
+            raise ValueError(msg)
+
+    def _get_item_impl(self, index: int) -> ClassificationHierarchicalSample | None:
+        item = self.dm_subset[index]
+        item.image = to_dtype(to_image(item.image), dtype=torch.float32)
+        item.label = torch.as_tensor(self._convert_label_to_hlabel_format([Label(label=idx) for idx in item.label], []))
+        return self._apply_transforms(item)
+
+    def _convert_label_to_hlabel_format(self, label_anns: list[Label], ignored_labels: list[int]) -> list[int]:
+        """Convert format of the label to the h-label.
+
+        It converts the label format to h-label format.
+        Total length of result is sum of number of hierarchy and number of multilabel classes.
+
+        i.e.
+        Let's assume that we used the same dataset with example of the definition of HLabelData
+        and the original labels are ["Rigid", "Triangle", "Lion"].
+
+        Then, h-label format will be [0, 1, 1, 0].
+        The first N-th indices represent the label index of multiclass heads (N=num_multiclass_heads),
+        others represent the multilabel labels.
+
+        [Multiclass Heads]
+        0-th index = 0 -> ["Rigid"(O), "Non-Rigid"(X)] <- First multiclass head
+        1-st index = 1 -> ["Rectangle"(O), "Triangle"(X), "Circle"(X)] <- Second multiclass head
+
+        [Multilabel Head]
+        2, 3 indices = [1, 0] -> ["Lion"(O), "Panda"(X)]
+        """
+        if not isinstance(self.label_info, HLabelInfo):
+            msg = f"The type of label_info should be HLabelInfo, got {type(self.label_info)}."
+            raise TypeError(msg)
+
+        num_multiclass_heads = self.label_info.num_multiclass_heads
+        num_multilabel_classes = self.label_info.num_multilabel_classes
+
+        class_indices = [0] * (num_multiclass_heads + num_multilabel_classes)
+        for i in range(num_multiclass_heads):
+            class_indices[i] = -1
+
+        for ann in label_anns:
+            if self.data_format == "arrow":
+                # skips unknown labels for instance, the empty one
+                if self.dm_categories.items[ann.label].name not in self.id_to_name_mapping:
+                    continue
+                ann_name = self.id_to_name_mapping[self.dm_categories.items[ann.label].name]
+            else:
+                ann_name = self.dm_categories.items[ann.label].name
+            group_idx, in_group_idx = self.label_info.class_to_group_idx[ann_name]
+
+            if group_idx < num_multiclass_heads:
+                class_indices[group_idx] = in_group_idx
+            elif ann.label not in ignored_labels:
+                class_indices[num_multiclass_heads + in_group_idx] = 1
+            else:
+                class_indices[num_multiclass_heads + in_group_idx] = -1
+
+        return class_indices
+
+    def get_idx_list_per_classes(self, use_string_label: bool = False) -> dict[int, list[int]]:
+        """Get a dictionary mapping class labels (string or int) to lists of samples.
+
+        Args:
+            use_string_label (bool): If True, use string class labels as keys.
+                If False, use integer indices as keys.
+        """
+        idx_list_per_classes: dict[int, list[int]] = {}
+        for idx in range(len(self)):
+            item = self.dm_subset[idx]
+            labels = item.label.tolist()
+            if use_string_label:
+                labels = [self.label_info.label_names[label] for label in labels]
+            for label in labels:
+                if label not in idx_list_per_classes:
+                    idx_list_per_classes[label] = []
+                idx_list_per_classes[label].append(idx)
         return idx_list_per_classes
--- library/src/otx/data/entity/sample.py
+++ library/src/otx/data/entity/sample.py
@@ -135,6 +135,42 @@ class ClassificationSample(OTXSample):
 
 
 @register_pytree_node
+class ClassificationMultiLabelSample(OTXSample):
+    """ClassificationMultiLabelSample is a base class for OTX multi label classification items."""
+
+    image: np.ndarray | tv_tensors.Image = image_field(dtype=pl.UInt8)
+    label: np.ndarray | torch.Tensor = label_field(pl.Int32(), multi_label=True)
+    dm_image_info: DmImageInfo = image_info_field()
+
+    def __post_init__(self) -> None:
+        shape = (self.dm_image_info.height, self.dm_image_info.width)
+
+        self.img_info = ImageInfo(
+            img_idx=0,
+            img_shape=shape,
+            ori_shape=shape,
+        )
+
+
+@register_pytree_node
+class ClassificationHierarchicalSample(OTXSample):
+    """ClassificationHierarchicalSample is a base class for OTX hierarchical classification items."""
+
+    image: np.ndarray | tv_tensors.Image = image_field(dtype=pl.UInt8)
+    label: np.ndarray | torch.Tensor = label_field(pl.Int32(), is_list=True)
+    dm_image_info: DmImageInfo = image_info_field()
+
+    def __post_init__(self) -> None:
+        shape = (self.dm_image_info.height, self.dm_image_info.width)
+
+        self.img_info = ImageInfo(
+            img_idx=0,
+            img_shape=shape,
+            ori_shape=shape,
+        )
+
+
+@register_pytree_node
 class DetectionSample(OTXSample):
     """DetectionSample is a base class for OTX detection items."""
 
