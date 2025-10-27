9d5c3d669023408905c38070c3467fb60ac86465
Leonardo Lai <leonardo.lai@intel.com>
2025-10-27 16:11:54 +0100

    use classmethod instead of staticmethod

--- application/backend/app/services/evaluation/factory.py
+++ application/backend/app/services/evaluation/factory.py
@@ -29,11 +29,11 @@ class EvaluatorFactory:
         InstanceSegmentationSample: InstanceSegmentationEvaluator,
     }
 
-    @staticmethod
-    def get_evaluator(predictions_dataset: Dataset, ground_truth_dataset: Dataset) -> Evaluator:
+    @classmethod
+    def get_evaluator(cls, predictions_dataset: Dataset, ground_truth_dataset: Dataset) -> Evaluator:
         if predictions_dataset.dtype != ground_truth_dataset.dtype:
             raise ValueError("Predictions and ground truth datasets must have the same dtype")
-        evaluator_cls = EvaluatorFactory._registry.get(predictions_dataset.dtype)
+        evaluator_cls = cls._registry.get(predictions_dataset.dtype)
         if evaluator_cls is None:
             raise NotImplementedError(f"Evaluator for dataset type {predictions_dataset.dtype} is not implemented")
         return evaluator_cls(predictions_dataset=predictions_dataset, ground_truth_dataset=ground_truth_dataset)
