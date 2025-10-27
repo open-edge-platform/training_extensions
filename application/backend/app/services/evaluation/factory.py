29f672fab444f9f8043ac0f5573db451a8dcf875
Leonardo Lai <leonardo.lai@intel.com>
2025-10-27 15:29:22 +0100

    simplify factory implementation by means of registry dict

--- application/backend/app/services/evaluation/factory.py
+++ application/backend/app/services/evaluation/factory.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2025 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from datumaro.experimental import Dataset
+from datumaro.experimental import Dataset, Sample
 
 from app.services.datumaro_converter import (
     ClassificationSample,
@@ -22,27 +22,18 @@ from .evaluators import (
 class EvaluatorFactory:
     """Factory to get a suitable evaluator for a given set of ground truth and predictions datasets."""
 
-    # TODO refactor with registry pattern
+    _registry: dict[type[Sample], type[Evaluator]] = {
+        ClassificationSample: MultiClassClassificationEvaluator,
+        MultilabelClassificationSample: MultiLabelClassificationEvaluator,
+        DetectionSample: DetectionEvaluator,
+        InstanceSegmentationSample: InstanceSegmentationEvaluator,
+    }
 
     @staticmethod
     def get_evaluator(predictions_dataset: Dataset, ground_truth_dataset: Dataset) -> Evaluator:
         if predictions_dataset.dtype != ground_truth_dataset.dtype:
             raise ValueError("Predictions and ground truth datasets must have the same dtype")
-
-        if predictions_dataset.dtype is ClassificationSample:
-            return MultiClassClassificationEvaluator(
-                predictions_dataset=predictions_dataset, ground_truth_dataset=ground_truth_dataset
-            )
-        if predictions_dataset.dtype is MultilabelClassificationSample:
-            return MultiLabelClassificationEvaluator(
-                predictions_dataset=predictions_dataset, ground_truth_dataset=ground_truth_dataset
-            )
-        if predictions_dataset.dtype is DetectionSample:
-            return DetectionEvaluator(
-                predictions_dataset=predictions_dataset, ground_truth_dataset=ground_truth_dataset
-            )
-        if predictions_dataset.dtype is InstanceSegmentationSample:
-            return InstanceSegmentationEvaluator(
-                predictions_dataset=predictions_dataset, ground_truth_dataset=ground_truth_dataset
-            )
-        raise NotImplementedError(f"Evaluator for dataset type {predictions_dataset.dtype} is not implemented")
+        evaluator_cls = EvaluatorFactory._registry.get(predictions_dataset.dtype)
+        if evaluator_cls is None:
+            raise NotImplementedError(f"Evaluator for dataset type {predictions_dataset.dtype} is not implemented")
+        return evaluator_cls(predictions_dataset=predictions_dataset, ground_truth_dataset=ground_truth_dataset)
