7f8efeb6a7621a925d0f8e9643947da969a4a782
Albert van Houten <albert.van.houten@intel.com>
2025-12-08 16:50:14 +0100

    Force multiprocessing start method.
    Signed-off-by: Albert van Houten <albert.van.houten@intel.com>

--- library/tests/conftest.py
+++ library/tests/conftest.py
@@ -48,7 +48,8 @@ def setup_test_environment():
 
     # Set multiprocessing method if not already set
     if multiprocessing.get_start_method(allow_none=True) is None:
-        multiprocessing.set_start_method("spawn")
+        multiprocessing.set_start_method("spawn", force=True)
+
     yield
 
     # Restore original working directory
