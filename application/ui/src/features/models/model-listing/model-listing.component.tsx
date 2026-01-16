949fe1a6daef4d935bd1493c5a2e066faeaf9367
Joao Vilaca <joao.vilaca@intel.com>
2026-01-16 10:52:12 +0100

    Improve sorting/grouping & add searchBy; Update tests

--- application/ui/src/features/models/model-listing/model-listing.component.tsx
+++ application/ui/src/features/models/model-listing/model-listing.component.tsx
@@ -3,12 +3,15 @@
 
 import { Divider, Flex, View } from '@geti/ui';
 
+import { EmptySearchResults } from './components/empty-search-results.component';
 import { GroupModelsContainer } from './components/group-models-container.component';
 import { Header } from './components/header.component';
 import { useModelListing } from './provider/model-listing-provider';
 
 export const ModelListing = () => {
-    const { groupedModels } = useModelListing();
+    const { groupedModels, searchBy } = useModelListing();
+
+    const hasNoResults = groupedModels.length === 0 && searchBy.length > 0;
 
     return (
         <View padding={'size-300'}>
@@ -16,15 +19,19 @@ export const ModelListing = () => {
 
             <Divider size={'S'} marginY={'size-300'} />
 
-            <Flex direction={'column'} gap={'size-300'}>
-                {groupedModels.map(({ group, models }, index) => (
-                    <GroupModelsContainer
-                        key={'id' in group ? group.id : `${group.name}-${index}`}
-                        group={group}
-                        models={models}
-                    />
-                ))}
-            </Flex>
+            {hasNoResults ? (
+                <EmptySearchResults />
+            ) : (
+                <Flex direction={'column'} gap={'size-300'}>
+                    {groupedModels.map(({ group, models }, index) => (
+                        <GroupModelsContainer
+                            key={'id' in group ? group.id : `${group.name}-${index}`}
+                            group={group}
+                            models={models}
+                        />
+                    ))}
+                </Flex>
+            )}
         </View>
     );
 };
