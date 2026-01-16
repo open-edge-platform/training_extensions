aaa18d2216024a3693e08c23318e56cf1c78cd1d
Joao Vilaca <joao.vilaca@intel.com>
2026-01-16 11:57:24 +0100

    Bonus: Add train model entry poitn

--- application/ui/src/features/models/model-listing/components/group-headers/dataset-group-header.component.tsx
+++ application/ui/src/features/models/model-listing/components/group-headers/dataset-group-header.component.tsx
@@ -4,6 +4,7 @@
 import { Button, dimensionValue, Flex, Grid, Heading, Text } from '@geti/ui';
 import { Image, Tag } from '@geti/ui/icons';
 
+import { TrainModel } from '../../../train-model/train-model';
 import type { DatasetGroup } from '../../types';
 import { ThreeSectionRange } from '../three-section-range.component';
 
@@ -50,7 +51,7 @@ export const DatasetGroupHeader = ({ dataset }: DatasetGroupHeaderProps) => {
             />
 
             <Flex>
-                <Button variant='primary'>Train model</Button>
+                <TrainModel />
             </Flex>
         </Grid>
     );
