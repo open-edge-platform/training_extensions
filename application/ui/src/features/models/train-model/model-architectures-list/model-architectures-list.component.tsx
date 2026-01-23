26d905d3822c2c28835435b20203014addd125ee
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-01-23 10:25:47 +0100

    feat: Display recommended architectures and simplify code (#5291)

--- application/ui/src/features/models/train-model/model-architectures-list/model-architectures-list.component.tsx
+++ application/ui/src/features/models/train-model/model-architectures-list/model-architectures-list.component.tsx
@@ -1,62 +1,74 @@
 // Copyright (C) 2025 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-import { useState } from 'react';
+import { ReactNode, useState } from 'react';
 
 import { Button, Flex } from '@geti/ui';
 
-import { type ModelArchitecture as ModelArchitectureType } from '../../../../constants/shared-types';
+import { ModelArchitectureWithPerformanceCategory } from '../../../../constants/shared-types';
+import { useTrainModel } from '../train-model-provider.component';
 import { AllModelArchitectures } from './all-model-architectures.component';
 import { RecommendedModelArchitectures } from './recommended-model-architectures.component';
 
-const getRecommendedArchitectures = (modelArchitectures: ModelArchitectureType[]) => {
+const getRecommendedArchitectures = (modelArchitectures: ModelArchitectureWithPerformanceCategory[]) => {
+    const recommended = modelArchitectures.filter(
+        (modelArchitecture) => modelArchitecture.performanceCategory !== undefined
+    );
+
+    if (recommended.length > 0) {
+        return recommended;
+    }
+
+    // For now just return top 3 recommended architectures, but in the future we can add more logic here
     return modelArchitectures.slice(0, 3);
 };
 
-type ModelArchitecturesListProps = {
-    activeModelArchitectureId: string | undefined;
-    modelArchitectures: ModelArchitectureType[];
-    selectedModelArchitectureId: string | null;
-    onSelectedModelArchitectureIdChange: (modelArchitectureId: string | null) => void;
+type ModelArchitecturesContainerProps = {
+    children: ReactNode;
+    showMore: boolean;
+    onShowMore: (showMore: boolean) => void;
 };
 
-export const ModelArchitecturesList = ({
-    modelArchitectures,
-    activeModelArchitectureId,
-    selectedModelArchitectureId,
-    onSelectedModelArchitectureIdChange,
-}: ModelArchitecturesListProps) => {
+const ModelArchitecturesContainer = ({ children, onShowMore, showMore }: ModelArchitecturesContainerProps) => {
+    return (
+        <Flex direction={'column'} minHeight={0} gap={'size-300'}>
+            {children}
+
+            <Button alignSelf={'start'} variant={'primary'} onPress={() => onShowMore(!showMore)}>
+                {showMore ? 'Show less' : 'Show more'}
+            </Button>
+        </Flex>
+    );
+};
+
+export const ModelArchitecturesList = () => {
     const [showMore, setShowMore] = useState<boolean>(false);
+    const { activeModelArchitectureId, modelArchitectures, selectedModelArchitectureId, onSelectModelArchitectureId } =
+        useTrainModel();
 
     if (showMore) {
         return (
-            <Flex direction={'column'} minHeight={0} gap={'size-300'}>
+            <ModelArchitecturesContainer showMore={showMore} onShowMore={setShowMore}>
                 <AllModelArchitectures
                     activeModelArchitectureId={activeModelArchitectureId}
                     modelArchitectures={modelArchitectures}
                     selectedModelArchitectureId={selectedModelArchitectureId}
-                    onSelectedModelArchitectureIdChange={onSelectedModelArchitectureIdChange}
+                    onSelectedModelArchitectureIdChange={onSelectModelArchitectureId}
                 />
-                <Button alignSelf={'start'} variant={'primary'} onPress={() => setShowMore(false)}>
-                    Show less
-                </Button>
-            </Flex>
+            </ModelArchitecturesContainer>
         );
     }
 
     const recommendedArchitectures = getRecommendedArchitectures(modelArchitectures);
 
     return (
-        <Flex direction={'column'} minHeight={0} gap={'size-300'}>
+        <ModelArchitecturesContainer showMore={showMore} onShowMore={setShowMore}>
             <RecommendedModelArchitectures
                 activeModelArchitectureId={activeModelArchitectureId}
                 modelArchitectures={recommendedArchitectures}
                 selectedModelArchitectureId={selectedModelArchitectureId}
-                onSelectedModelArchitectureIdChange={onSelectedModelArchitectureIdChange}
+                onSelectedModelArchitectureIdChange={onSelectModelArchitectureId}
             />
-            <Button alignSelf={'start'} variant={'primary'} onPress={() => setShowMore(true)}>
-                Show more
-            </Button>
-        </Flex>
+        </ModelArchitecturesContainer>
     );
 };
--- application/ui/src/features/models/train-model/select-training-device.component.tsx
+++ application/ui/src/features/models/train-model/select-training-device.component.tsx
@@ -3,26 +3,19 @@
 
 import { Item, Picker } from '@geti/ui';
 
-import type { DeviceType, TrainingDevices } from '../../../constants/shared-types';
+import type { DeviceType } from '../../../constants/shared-types';
+import { useTrainModel } from './train-model-provider.component';
 
-type SelectTrainingDeviceProps = {
-    trainingDevices: TrainingDevices[];
-    selectedTrainingDevice: DeviceType | null;
-    onSelectedTrainingDeviceChange: (trainingDeviceId: DeviceType | null) => void;
-};
+export const SelectTrainingDevice = () => {
+    const { trainingDevices, onSelectTrainingDevice, selectedTrainingDevice } = useTrainModel();
 
-export const SelectTrainingDevice = ({
-    trainingDevices,
-    onSelectedTrainingDeviceChange,
-    selectedTrainingDevice,
-}: SelectTrainingDeviceProps) => {
     return (
         <Picker
             flex={1}
             items={trainingDevices}
             label={'Select training device'}
             selectedKey={selectedTrainingDevice}
-            onSelectionChange={(key) => onSelectedTrainingDeviceChange(key as DeviceType)}
+            onSelectionChange={(key) => onSelectTrainingDevice(key as DeviceType)}
         >
             {(item) => <Item key={item.type}>{item.name}</Item>}
         </Picker>
