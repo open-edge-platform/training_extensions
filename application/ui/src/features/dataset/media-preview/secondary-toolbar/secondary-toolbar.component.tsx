b17db7a658fb1d1880786bdf828d29185a542da2
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-01-26 17:22:31 +0100

    update styles of close button

--- application/ui/src/features/dataset/media-preview/secondary-toolbar/secondary-toolbar.component.tsx
+++ application/ui/src/features/dataset/media-preview/secondary-toolbar/secondary-toolbar.component.tsx
@@ -1,7 +1,8 @@
 // Copyright (C) 2025 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-import { Button, ButtonGroup, dimensionValue, Flex, Grid, Key } from '@geti/ui';
+import { ActionButton, Button, ButtonGroup, dimensionValue, Flex, Key, Text } from '@geti/ui';
+import { CloseSemiBold } from '@geti/ui/icons';
 import { useQueryClient, type QueryClient } from '@tanstack/react-query';
 import { isEmpty } from 'lodash-es';
 
@@ -13,9 +14,10 @@ import { DeleteMediaItem } from '../../gallery/delete-media-item/delete-media-it
 import { AnnotatorModes } from './annotator-modes/annotator-modes-toggle.component';
 import type { AnnotatorMode } from './annotator-modes/mode';
 import { LabelPicker } from './label-picker.component';
+import { ToolbarContainer } from './toolbar-container/toolbar-container.component';
 import { useSecondaryToolbarState } from './use-secondary-toolbar-state.hook';
 
-import classes from '../media-preview.module.scss';
+import styles from './secondary-toolbar.module.scss';
 
 type SecondaryToolbarProps = {
     items: Media[];
@@ -88,35 +90,43 @@ export const SecondaryToolbar = ({
             height={'100%'}
             width={'100%'}
             alignItems={'center'}
+            justifyContent={'space-between'}
             UNSAFE_style={{ paddingTop: dimensionValue('size-125') }}
         >
-            <Grid width={'100%'} UNSAFE_className={classes.toolbarGrid} isHidden={isHidden}>
-                <Flex width={'100%'} UNSAFE_className={classes.toolbarSection} justifyContent={'space-between'}>
-                    <AnnotatorModes mode={mode} onModeChange={onModeChange} />
+            <ToolbarContainer isHidden={!hasAnnotations}>
+                <AnnotatorModes mode={mode} onModeChange={onModeChange} />
+            </ToolbarContainer>
+            <ToolbarContainer isHidden={isHidden}>
+                <LabelPicker selectedLabel={selectedLabel} labels={projectLabels} onSelect={handleSelect} />
+            </ToolbarContainer>
+            <ToolbarContainer>
+                <ButtonGroup>
+                    <DeleteMediaItem
+                        itemsIds={[String(mediaItem.id)]}
+                        onDeleted={([deletedItem]: string[]) => handleDeleteItem([deletedItem], items.length - 1)}
+                    />
+                    <Button
+                        variant='accent'
+                        onPress={handleSubmit}
+                        isPending={isSaving}
+                        marginStart={'size-200'}
+                        isDisabled={!hasAnnotations || isSaving}
+                    >
+                        Submit
+                    </Button>
 
-                    <LabelPicker selectedLabel={selectedLabel} labels={projectLabels} onSelect={handleSelect} />
-
-                    <ButtonGroup>
-                        <DeleteMediaItem
-                            itemsIds={[String(mediaItem.id)]}
-                            onDeleted={([deletedItem]: string[]) => handleDeleteItem([deletedItem], items.length - 1)}
-                        />
-                        <Button
-                            variant='accent'
-                            onPress={handleSubmit}
-                            isPending={isSaving}
-                            marginStart={'size-200'}
-                            isDisabled={!hasAnnotations || isSaving}
-                        >
-                            Submit
-                        </Button>
-
-                        <Button variant='secondary' onPress={onClose} isDisabled={isSaving}>
-                            Close
-                        </Button>
-                    </ButtonGroup>
-                </Flex>
-            </Grid>
+                    <ActionButton
+                        isQuiet
+                        onPress={onClose}
+                        isDisabled={isSaving}
+                        marginStart={'size-100'}
+                        UNSAFE_className={styles.closeButton}
+                    >
+                        <CloseSemiBold width={14} height={14} />
+                        <Text>Close</Text>
+                    </ActionButton>
+                </ButtonGroup>
+            </ToolbarContainer>
         </Flex>
     );
 };
