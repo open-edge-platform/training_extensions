56181a7f7b3e9c2d8ba1ee91e9612aa48e766682
Joao Vilaca <joao.vilaca@intel.com>
2026-02-04 15:20:02 +0100

    Move annotator providers to its own component

---
+++ application/ui/src/features/dataset/media-preview/annotator-providers.component.tsx
@@ -0,0 +1,52 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import { ReactNode } from 'react';
+
+import { ZoomProvider } from '../../../components/zoom/zoom.provider';
+import type { AnnotationDTO, Media } from '../../../constants/shared-types';
+import { AnnotationActionsProvider } from '../../../shared/annotator/annotation-actions-provider.component';
+import { AnnotationVisibilityProvider } from '../../../shared/annotator/annotation-visibility-provider.component';
+import { AnnotatorProvider } from '../../../shared/annotator/annotator-provider.component';
+import { SelectAnnotationProvider } from '../../../shared/annotator/select-annotation-provider.component';
+import { CanvasSettingsProvider } from './primary-toolbar/settings/canvas-settings-provider.component';
+import type { AnnotatorMode } from './secondary-toolbar/annotator-modes/mode';
+
+type AnnotatorProvidersProps = {
+    mediaItem: Media;
+    initialAnnotationsDTO: AnnotationDTO[];
+    initialPredictionsDTO: AnnotationDTO[];
+    isUserReviewed: boolean;
+    mode: AnnotatorMode;
+    children: ReactNode;
+};
+
+export const AnnotatorProviders = ({
+    mediaItem,
+    initialAnnotationsDTO,
+    initialPredictionsDTO,
+    isUserReviewed,
+    mode,
+    children,
+}: AnnotatorProvidersProps) => {
+    return (
+        <ZoomProvider>
+            <AnnotationActionsProvider
+                key={mediaItem.id}
+                mediaItem={mediaItem}
+                initialAnnotationsDTO={initialAnnotationsDTO}
+                initialPredictionsDTO={initialPredictionsDTO}
+                isUserReviewed={isUserReviewed}
+                mode={mode}
+            >
+                <SelectAnnotationProvider>
+                    <AnnotationVisibilityProvider>
+                        <AnnotatorProvider mediaItem={mediaItem}>
+                            <CanvasSettingsProvider>{children}</CanvasSettingsProvider>
+                        </AnnotatorProvider>
+                    </AnnotationVisibilityProvider>
+                </SelectAnnotationProvider>
+            </AnnotationActionsProvider>
+        </ZoomProvider>
+    );
+};
