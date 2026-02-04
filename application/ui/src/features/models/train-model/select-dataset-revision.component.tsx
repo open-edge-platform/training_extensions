be308f085a183e2abb1bef3016e62448b4f64b0b
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-02-04 11:09:51 +0100

    feat: Add 'Use current revision' for the modal training (#5414)

--- application/ui/src/features/models/train-model/select-dataset-revision.component.tsx
+++ application/ui/src/features/models/train-model/select-dataset-revision.component.tsx
@@ -6,15 +6,15 @@ import { Item, Picker } from '@geti/ui';
 import { useTrainModel } from './train-model-provider.component';
 
 export const SelectDatasetRevision = () => {
-    const { datasetRevisions, selectedDatasetRevision, onSelectDatasetRevision } = useTrainModel();
+    const { datasetRevisions, selectedDatasetRevisionId, onSelectDatasetRevisionId } = useTrainModel();
 
     return (
         <Picker
             flex={1}
             items={datasetRevisions}
             label={'Select dataset revision'}
-            selectedKey={selectedDatasetRevision}
-            onSelectionChange={(key) => onSelectDatasetRevision(String(key))}
+            selectedKey={selectedDatasetRevisionId}
+            onSelectionChange={(key) => onSelectDatasetRevisionId(String(key))}
         >
             {(item) => <Item key={item.id}>{item.name}</Item>}
         </Picker>
