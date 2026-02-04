93e14afa76df71c1231d0c0694ae0392f3789a17
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-02-04 14:05:16 +0100

    feat: Disable training model button (#5420)

---
+++ application/ui/src/features/models/hooks/use-is-training-button-disabled.ts
@@ -0,0 +1,30 @@
+// Copyright (C) 2025-2026 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import { useProjectIdentifier } from 'hooks/use-project-identifier.hook';
+
+import { $api } from '../../../api/client';
+
+const useGetDatasetItems = () => {
+    const projectId = useProjectIdentifier();
+    return $api.useQuery('get', '/api/projects/{project_id}/dataset/items', {
+        params: {
+            path: {
+                project_id: projectId,
+            },
+            query: {
+                limit: 5,
+                annotation_status: 'reviewed',
+            },
+        },
+    });
+};
+
+const MIN_NUMBER_OF_ANNOTATED_ITEMS = 3;
+
+export const useIsTrainingButtonDisabled = () => {
+    const { data: datasetItems } = useGetDatasetItems();
+    const count = datasetItems?.pagination.total ?? 0;
+
+    return count < MIN_NUMBER_OF_ANNOTATED_ITEMS;
+};
