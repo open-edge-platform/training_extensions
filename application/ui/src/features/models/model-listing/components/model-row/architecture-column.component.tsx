33f1b0a2b2de9cfe88e8ccf3a185f55cc54adf0d
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-02-06 08:39:54 +0100

    rename styles to classes, remove default align items

--- application/ui/src/features/models/model-listing/components/model-row/architecture-column.component.tsx
+++ application/ui/src/features/models/model-listing/components/model-row/architecture-column.component.tsx
@@ -6,7 +6,7 @@ import { Flex, Text } from '@geti/ui';
 import { ReactComponent as ThumbsUp } from '../../../../../assets/icons/thumbs-up.svg';
 import { ModelBadge } from './model-badge.component';
 
-import styles from './model-row.module.scss';
+import classes from './model-row.module.scss';
 
 type ArchitectureColumnProps = {
     architecture: string;
@@ -14,8 +14,8 @@ type ArchitectureColumnProps = {
 
 export const ArchitectureColumn = ({ architecture }: ArchitectureColumnProps) => {
     return (
-        <Flex alignItems={'start'} direction={'column'} gap={'size-100'}>
-            <Text UNSAFE_className={styles.smallText}>{architecture} (Apache 2.0)</Text>
+        <Flex direction={'column'} gap={'size-100'}>
+            <Text UNSAFE_className={classes.smallText}>{architecture} (Apache 2.0)</Text>
             {/* TODO: Speed is hardcoded for now, once the backend is update we need to update this */}
             <ModelBadge id={'architecture-name'}>
                 <ThumbsUp />
