8470e1e09aeb40d094b4841da421fb7f5037a426
Joao Vilaca <joao.vilaca@intel.com>
2026-02-09 16:17:23 +0100

    Update dataset revision picker

--- application/ui/src/features/models/train-model/select-dataset-revision.component.tsx
+++ application/ui/src/features/models/train-model/select-dataset-revision.component.tsx
@@ -1,7 +1,7 @@
 // Copyright (C) 2025 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-import { Item, Picker } from '@geti/ui';
+import { Content, ContextualHelp, Flex, Heading, Item, Picker } from '@geti/ui';
 
 import { useTrainModel } from './train-model-provider.component';
 
@@ -9,14 +9,26 @@ export const SelectDatasetRevision = () => {
     const { datasetRevisions, selectedDatasetRevisionId, onSelectDatasetRevisionId } = useTrainModel();
 
     return (
-        <Picker
-            flex={1}
-            items={datasetRevisions}
-            label={'Select dataset revision'}
-            selectedKey={selectedDatasetRevisionId}
-            onSelectionChange={(key) => onSelectDatasetRevisionId(String(key))}
-        >
-            {(item) => <Item key={item.id}>{item.name}</Item>}
-        </Picker>
+        <Flex alignItems={'center'} gap={'size-100'}>
+            <Picker
+                flex={1}
+                items={datasetRevisions}
+                label={'Select dataset'}
+                selectedKey={selectedDatasetRevisionId}
+                onSelectionChange={(key) => onSelectDatasetRevisionId(String(key))}
+            >
+                {(item) => <Item key={item.id}>{item.name}</Item>}
+            </Picker>
+
+            <ContextualHelp variant={'info'} marginTop={'size-300'}>
+                <Heading>Selecting a dataset</Heading>
+                <Content>
+                    Choose the version of the dataset to use for training. If you want to train the new model on the
+                    exact same data (media and annotations) as another model, please select the corresponding dataset
+                    revision. Conversely, if you want to train on the most recent version of the data (what you see in
+                    the &ldquo;Dataset&rdquo; page), please select &ldquo;Use current dataset&rdquo;.
+                </Content>
+            </ContextualHelp>
+        </Flex>
     );
 };
