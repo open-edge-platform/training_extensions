a2d3acac1ffe8d91620c151fde7928ddaced67f5
Joao Vilaca <joao.vilaca@intel.com>
2026-02-11 08:16:38 +0100

    Add component test

--- application/ui/src/features/annotator/annotations/annotations.component.tsx
+++ application/ui/src/features/annotator/annotations/annotations.component.tsx
@@ -32,7 +32,7 @@ export const Annotations = ({ annotations, width, height, isFocussed }: Annotati
     return (
         <svg
             aria-label={'annotations'}
-            data-testid={'annotations-canvas-tools'}
+            data-testid={'annotation-list'}
             width={width}
             height={height}
             tabIndex={-1}
--- application/ui/tests/datasets/annotator-page.ts
+++ application/ui/tests/datasets/annotator-page.ts
@@ -90,7 +90,7 @@ export class AnnotatorPage {
     }
 
     getAnnotationsList() {
-        return this.page.getByTestId('annotations-canvas-tools');
+        return this.page.getByTestId('annotation-list');
     }
 
     async getAnnotationsListItems(label: 'annotation rect' | 'prediction rect') {
