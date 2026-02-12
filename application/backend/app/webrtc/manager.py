eb4a29d6f4277f37918d06f5f129fd7f2a4478fb
Jort Bergfeld <jort.bergfeld@intel.com>
2026-02-12 14:42:36 +0100

    Remove unnecessary list
    Signed-off-by: Jort Bergfeld <jort.bergfeld@intel.com>

--- application/backend/app/webrtc/manager.py
+++ application/backend/app/webrtc/manager.py
@@ -83,7 +83,7 @@ class WebRTCManager:
     async def cleanup(self) -> None:
         """Clean up all connections"""
         async with self._lock:
-            for webrtc_id, pc in list(self._pcs.items()):
+            for webrtc_id, pc in self._pcs.items():
                 await pc.close()
                 self._frame_broadcaster.unregister(webrtc_id=webrtc_id)
             self._pcs.clear()
