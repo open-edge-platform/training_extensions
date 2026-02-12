d082d3e9bf9c389582cafa60d9dfd3e5f8fddb9a
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-02-12 10:22:00 +0100

    refactor: Draw already fetched img on <canvas /> instead of <img />

--- application/ui/src/features/annotator/annotator-canvas/annotator-canvas.tsx
+++ application/ui/src/features/annotator/annotator-canvas/annotator-canvas.tsx
@@ -1,7 +1,7 @@
 // Copyright (C) 2025 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-import { MouseEvent } from 'react';
+import { MouseEvent, useCallback } from 'react';
 
 import { useProjectIdentifier } from 'hooks/use-project-identifier.hook';
 
@@ -10,25 +10,35 @@ import { ZoomTransform } from '../../../components/zoom/zoom-transform';
 import type { Media } from '../../../constants/shared-types';
 import { useAnnotationActions } from '../../../shared/annotator/annotation-actions-provider.component';
 import { useAnnotationVisibility } from '../../../shared/annotator/annotation-visibility-provider.component';
+import { useAnnotator } from '../../../shared/annotator/annotator-provider.component';
 import { useSelectedAnnotations } from '../../../shared/annotator/select-annotation-provider.component';
 import { Annotations } from '../annotations/annotations.component';
 import { ToolManager } from '../tools/tool-manager.component';
 
-import styles from './annotator-canvas.module.scss';
-
-const getImageUrl = (projectId: string, itemId: string) => {
-    return `${API_BASE_URL}/api/projects/${projectId}/dataset/media/${itemId}/binary`;
-};
+import classes from './annotator-canvas.module.scss';
 
 type AnnotatorCanvasProps = {
     mediaItem: Media;
 };
 
 export const AnnotatorCanvas = ({ mediaItem }: AnnotatorCanvasProps) => {
-    const project_id = useProjectIdentifier();
     const { annotations } = useAnnotationActions();
     const { selectedAnnotations } = useSelectedAnnotations();
     const { isFocussed } = useAnnotationVisibility();
+    const { image } = useAnnotator();
+
+    const drawImageOnCanvas = useCallback(
+        (ref: HTMLCanvasElement | null) => {
+            const ctx = ref?.getContext('2d');
+
+            if (ctx == null) {
+                return;
+            }
+
+            ctx.putImageData(image, 0, 0);
+        },
+        [image]
+    );
 
     // Order annotations by selection. Selected annotation should always be on top.
     const orderedAnnotations = [
@@ -44,11 +54,7 @@ export const AnnotatorCanvas = ({ mediaItem }: AnnotatorCanvasProps) => {
                 style={{ position: 'relative', height: '100%', width: '100%' }}
                 onContextMenu={(event: MouseEvent): void => event.preventDefault()}
             >
-                <img
-                    src={getImageUrl(project_id, String(mediaItem.id))}
-                    alt='Collected data'
-                    className={styles.image}
-                />
+                <canvas ref={drawImageOnCanvas} width={image.width} height={image.height} className={classes.image} />
 
                 <Annotations
                     width={size.width}
