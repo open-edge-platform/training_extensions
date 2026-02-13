dfbf0d97829e0d03d05981d833fda123b81c12a5
João Vilaça <joao.vilaca@intel.com>
2026-02-13 09:34:15 +0100

    Recover from failed stream (#5505)

--- application/ui/src/features/models/model-listing/utils/model-transforms.ts
+++ application/ui/src/features/models/model-listing/utils/model-transforms.ts
@@ -7,13 +7,17 @@ import type { DatasetRevision, Model } from '../../../../constants/shared-types'
 import type { GroupByMode, GroupedModels, SortBy } from '../types';
 import { groupModelsByArchitecture, groupModelsByDataset } from './grouping';
 import { sortModels } from './sorting';
-import { isFailedModel } from './utils';
+import { isFailedModel, isTrainingModel } from './utils';
 
 export const filterBySearch = (models: Model[], query: string): Model[] =>
     query ? models.filter((model) => model.name?.toLowerCase().includes(query.toLowerCase())) : models;
 
 export const filterOutFailedModels = (models: Model[]): Model[] => {
     return models.filter((model) => !isFailedModel(model));
+};
+
+export const filterOutTrainingModels = (models: Model[]): Model[] => {
+    return models.filter((model) => !isTrainingModel(model));
 };
 
 export const groupModels = (
