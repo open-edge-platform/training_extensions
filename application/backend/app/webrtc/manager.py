8867871f33c884523707b5f2cc253b92a634879a
Jort Bergfeld <jort.bergfeld@intel.com>
2026-02-13 08:56:26 +0100

    Fix lock too much code
    Signed-off-by: Jort Bergfeld <jort.bergfeld@intel.com>

--- application/backend/app/webrtc/manager.py
+++ application/backend/app/webrtc/manager.py
@@ -34,33 +34,34 @@ class WebRTCManager:
 
     async def handle_offer(self, offer: Offer) -> Answer:
         """Create an SDP offer for a new WebRTC connection."""
+        pc = RTCPeerConnection(configuration=self._settings.config)
+
         async with self._lock:
-            pc = RTCPeerConnection(configuration=self._settings.config)
             self._pcs[offer.webrtc_id] = pc
 
-            # Add video track
-            stream_queue = self._frame_broadcaster.register(webrtc_id=offer.webrtc_id)
-            track = InferenceVideoStreamTrack(stream_queue=stream_queue)
-            pc.addTrack(track)
+        # Add video track
+        stream_queue = self._frame_broadcaster.register(webrtc_id=offer.webrtc_id)
+        track = InferenceVideoStreamTrack(stream_queue=stream_queue)
+        pc.addTrack(track)
 
-            @pc.on("connectionstatechange")
-            async def connection_state_change() -> None:
-                if pc.connectionState in ["failed", "closed"]:
-                    await self.cleanup_connection(offer.webrtc_id)
+        @pc.on("connectionstatechange")
+        async def connection_state_change() -> None:
+            if pc.connectionState in ["failed", "closed"]:
+                await self.cleanup_connection(offer.webrtc_id)
 
-            # Set remote description from client's offer
-            await pc.setRemoteDescription(RTCSessionDescription(sdp=offer.sdp, type=offer.type))
+        # Set remote description from client's offer
+        await pc.setRemoteDescription(RTCSessionDescription(sdp=offer.sdp, type=offer.type))
 
-            # Create answer
-            answer = await pc.createAnswer()
-            await pc.setLocalDescription(answer)
+        # Create answer
+        answer = await pc.createAnswer()
+        await pc.setLocalDescription(answer)
 
-            # Mangle SDP if public IP is configured
-            sdp = pc.localDescription.sdp
-            if self._settings.advertise_ip:
-                sdp = await self._sdp_handler.mangle_sdp(sdp, self._settings.advertise_ip)
+        # Mangle SDP if public IP is configured
+        sdp = pc.localDescription.sdp
+        if self._settings.advertise_ip:
+            sdp = await self._sdp_handler.mangle_sdp(sdp, self._settings.advertise_ip)
 
-            return Answer(sdp=sdp, type=pc.localDescription.type)
+        return Answer(sdp=sdp, type=pc.localDescription.type)
 
     def set_input(self, data: InputData) -> None:
         """Set input data for specific WebRTC connection"""
