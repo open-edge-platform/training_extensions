ca476291f739ec050c1c3665e503c2e0aa5c347a
Joao Vilaca <joao.vilaca@intel.com>
2026-02-13 08:37:06 +0100

    Add mocks for localStorage

--- application/ui/src/setup-tests.ts
+++ application/ui/src/setup-tests.ts
@@ -43,3 +43,24 @@ class ResizeObserverMock {
 }
 
 global.ResizeObserver = ResizeObserverMock;
+
+const localStorageMock = (() => {
+    let store: Record<string, string> = {};
+
+    return {
+        getItem: (key: string) => store[key] || null,
+        setItem: (key: string, value: string) => {
+            store[key] = value.toString();
+        },
+        removeItem: (key: string) => {
+            delete store[key];
+        },
+        clear: () => {
+            store = {};
+        },
+    };
+})();
+
+Object.defineProperty(global, 'localStorage', {
+    value: localStorageMock,
+});
