70c6338e3fc183a5365b843d22f4c489cf799785
Camilo <camilo.colorado@intel.com>
2026-02-19 14:50:25 +0100

    feat: Export Dialog - P4 (#5559)

--- application/ui/src/features/dataset/import-export/export-jobs-list/export-job/export-details/export-details.component.tsx
+++ application/ui/src/features/dataset/import-export/export-jobs-list/export-job/export-details/export-details.component.tsx
@@ -1,31 +1,37 @@
 // Copyright (C) 2025 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-import { Divider, Grid, Text } from '@geti/ui';
+import { dimensionValue, Divider, Grid, Text } from '@geti/ui';
 import { isEmpty } from 'lodash-es';
 
-import { ExportDatasetMetadata } from '../../../../../constants/shared-types';
-import { useProject } from '../../../../../hooks/api/project.hook';
+import { ExportDatasetMetadata } from '../../../../../../constants/shared-types';
+import { useProject } from '../../../../../../hooks/api/project.hook';
 
 type ExportJobDetailsProps = {
     metadata: ExportDatasetMetadata;
 };
 export const ExportJobDetails = ({ metadata }: ExportJobDetailsProps) => {
     const { data: selectedProject } = useProject();
-    const projectLabels = selectedProject.task.labels ?? [];
-    const exportLabels = metadata.filters.labels ?? [];
 
-    const labelsNames = exportLabels
-        .map((labelId) => projectLabels.find((label) => label.id === labelId)?.name)
-        .filter(Boolean);
+    const projectLabels = selectedProject.task.labels ?? [];
+    const exportLabelsNames = metadata.filters.labels ?? [];
+
+    const projectLabelsNames = projectLabels.map((label) => label.name);
+    const selectedLabels = exportLabelsNames.filter((name) => projectLabelsNames.includes(name));
+
+    const labelsList = isEmpty(selectedLabels) ? projectLabelsNames : selectedLabels;
 
     return (
-        <Grid gap='size-125' columns={['auto', '1px', '1fr', '1px', 'auto']}>
+        <Grid
+            gap='size-125'
+            columns={['auto', '1px', '1fr', '1px', 'auto']}
+            UNSAFE_style={{ fontWeight: 500, fontSize: dimensionValue('size-200') }}
+        >
             <Text>Export dataset - {metadata.export_format} format</Text>
 
             <Divider orientation='vertical' size='S' />
             <Text UNSAFE_style={{ overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}>
-                {isEmpty(labelsNames) ? 'Non labels included' : `Included images by label: ${labelsNames.join(', ')}`}
+                {`Included images by label: ${labelsList.join(', ')}`}
             </Text>
 
             {metadata.filters.include_unannotated === false && (
