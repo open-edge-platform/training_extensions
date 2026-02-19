2a4f7c3d3720dfcb3c57e455b113caff44af4e61
Evgeny Romanov <evgeny.romanov@intel.com>
2026-02-19 16:34:26 +0100

    Address frames using indexes instead of timestamps (#5567)

--- application/backend/app/alembic/versions/2d2b0c9a5c2c_schema.py
+++ application/backend/app/alembic/versions/2d2b0c9a5c2c_schema.py
@@ -147,7 +147,7 @@ def upgrade() -> None:
         "video_frames",
         sa.Column("id", sa.Text(), nullable=False),
         sa.Column("video_id", sa.Text(), nullable=False),
-        sa.Column("timestamp", sa.Float(), nullable=False),
+        sa.Column("frame_index", sa.Integer(), nullable=False),
         sa.Column("created_at", sa.DateTime(), server_default=sa.text("(CURRENT_TIMESTAMP)"), nullable=False),
         sa.Column("updated_at", sa.DateTime(), server_default=sa.text("(CURRENT_TIMESTAMP)"), nullable=False),
         sa.ForeignKeyConstraint(["id"], ["media.id"], ondelete="CASCADE"),
@@ -155,7 +155,7 @@ def upgrade() -> None:
         sa.PrimaryKeyConstraint("id"),
     )
     op.create_index("idx_video_frames_video_id", "video_frames", ["video_id"], unique=False)
-    op.create_index("idx_video_frames_video_id_timestamp", "video_frames", ["video_id", "timestamp"], unique=False)
+    op.create_index("idx_video_frames_video_id_frame_index", "video_frames", ["video_id", "frame_index"], unique=False)
     op.create_table(
         "dataset_items",
         sa.Column("id", sa.Text(), nullable=False),
@@ -237,7 +237,7 @@ def downgrade() -> None:
     op.drop_table("evaluations")
     op.drop_index("idx_dataset_items_user_reviewed", table_name="dataset_items")
     op.drop_table("dataset_items")
-    op.drop_index("idx_video_frames_video_id_timestamp", table_name="video_frames")
+    op.drop_index("idx_video_frames_video_id_frame_index", table_name="video_frames")
     op.drop_index("idx_video_frames_video_id", table_name="video_frames")
     op.drop_table("video_frames")
     op.drop_index("idx_model_revisions_project_status", table_name="model_revisions")
