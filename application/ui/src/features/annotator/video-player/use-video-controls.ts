f913d5911c51919297608c1fb511ce45b7e3c1a2
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-02-23 17:03:48 +0100

    feat: add more navigation logic to video controls

--- application/ui/src/features/annotator/video-player/use-video-controls.ts
+++ application/ui/src/features/annotator/video-player/use-video-controls.ts
@@ -3,7 +3,7 @@
 
 import { RefObject, useState } from 'react';
 
-import type { MediaVideo } from '../../../constants/shared-types';
+import type { MediaVideoFrame } from '../../../constants/shared-types';
 
 export type VideoControls = {
     canSelectPreviousFrame: boolean;
@@ -19,9 +19,33 @@ export type VideoControls = {
 
 export const useVideoControls = (
     videoRef: RefObject<HTMLVideoElement | null>,
-    mediaItem: MediaVideo | undefined
+    mediaItem: MediaVideoFrame | undefined,
+    selectVideoFrame: (media: MediaVideoFrame) => void,
+    changeCurrentFrameIndex: (index: number) => void
 ): VideoControls => {
     const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+    const totalFrames = mediaItem?.frame_count ?? 1;
+    const step = mediaItem?.frame_stride;
+    const currentFrameNumber = mediaItem.frame_number;
+
+    const round = (x: number) => Math.round(x / step) * step;
+    const previousVideoFrameNumber = round(currentFrameNumber - step);
+    const canSelectPreviousFrame = previousVideoFrameNumber >= 0;
+
+    const nextVideoFrameNumber = round(currentFrameNumber + step);
+    const canSelectNextFrame = nextVideoFrameNumber < totalFrames;
+
+    const selectFrame = (frameNumber: number) => {
+        if (videoRef.current === null) {
+            return;
+        }
+        // TODO: Update selected video frame in selected media item provider
+        selectVideoFrame({ ...mediaItem, frame_number: frameNumber });
+        changeCurrentFrameIndex(frameNumber);
+
+        videoRef.current.currentTime = (frameNumber + 1) / mediaItem.fps;
+    };
 
     const play = async () => {
         if (videoRef.current === null) {
@@ -42,25 +66,26 @@ export const useVideoControls = (
         if (videoRef.current === null) {
             return;
         }
+
         setIsPlaying(false);
         videoRef.current.pause();
+
+        const maxNearestFrame = Math.floor((mediaItem.frame_count - 1) / step) * step;
+        const nearestFrame = Math.min(maxNearestFrame, Math.round(currentFrameNumber / step) * step);
+
+        goto(nearestFrame);
     };
-
-    const frames = mediaItem?.frame_count ?? 1;
-    const fps = mediaItem?.fps || 1;
-    const step = 1 / fps;
-    const currentTime = videoRef.current?.currentTime ?? 0;
-
-    // TODO: These will change once API for video frames is supported
-    const canSelectPreviousFrame = currentTime - step >= 0;
-    const canSelectNextFrame = currentTime + step <= frames / fps;
 
     const nextFrame = () => {
         if (!canSelectNextFrame || videoRef.current === null) {
             return;
         }
 
-        videoRef.current.currentTime = videoRef.current.currentTime + step;
+        if (!isPlaying) {
+            selectFrame(nextVideoFrameNumber);
+        } else {
+            videoRef.current.currentTime += 1;
+        }
     };
 
     const previousFrame = () => {
@@ -68,11 +93,31 @@ export const useVideoControls = (
             return;
         }
 
-        videoRef.current.currentTime = videoRef.current.currentTime - step;
+        if (!isPlaying) {
+            selectFrame(previousVideoFrameNumber);
+        } else {
+            videoRef.current.currentTime -= 1;
+        }
     };
 
-    const goto = (_frameNumber: number) => {
-        //
+    const goto = (frameNumber: number) => {
+        if (videoRef.current === null) {
+            return;
+        }
+
+        if (frameNumber >= totalFrames || frameNumber < 0) {
+            return;
+        }
+
+        if (isPlaying) {
+            videoRef.current.pause();
+            setIsPlaying(false);
+        }
+
+        videoRef.current.currentTime = (frameNumber + 1) / mediaItem.fps;
+        const nearest = Math.min(Math.round(frameNumber / step) * step, totalFrames - 1);
+
+        selectFrame(nearest);
     };
 
     return {
