a132044ce0991164062adae5e29ff6bbeeea33dd
Colorado, Camilo <camilo.colorado@intel.com>
2026-03-03 21:15:43 +0100

    show import warnings

---
+++ application/ui/src/features/dataset/import-export/import-dataset/label-mapping/format-warning/format-warning.component.tsx
@@ -0,0 +1,55 @@
+// Copyright (C) 2026 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import { Divider, Flex, Text } from '@geti/ui';
+import { Alert } from '@geti/ui/icons';
+import { isNil } from 'lodash-es';
+
+import { components } from '../../../../../../api/openapi-spec';
+import { useProject } from '../../../../../../hooks/api/project.hook';
+
+import classes from './format-warning.module.scss';
+
+type AnnotationType = components['schemas']['AnnotationType'];
+
+type FormatWarningProps = {
+    annotationType?: AnnotationType;
+};
+
+const getMessage = (taskType: string, annotationType?: AnnotationType) => {
+    if (annotationType === 'bounding_box' && taskType === 'instance_segmentation') {
+        // eslint-disable-next-line max-len
+        return 'Imported dataset uses bounding box annotations, but your project’s dataset uses polygons. Annotations will be automatically converted to polygons to keep everything compatible.';
+    }
+
+    if (annotationType === 'polygon' && taskType === 'detection') {
+        // eslint-disable-next-line max-len
+        return 'Imported dataset uses polygon annotations, but your project’s dataset uses bounding boxes. Annotations will be automatically converted to bounding boxes to keep everything compatible.';
+    }
+
+    return null;
+};
+
+export const FormatWarning = ({ annotationType }: FormatWarningProps) => {
+    const { data: selectedProject } = useProject();
+
+    const message = getMessage(selectedProject?.task?.task_type, annotationType);
+
+    if (isNil(annotationType) || isNil(message)) {
+        return null;
+    }
+
+    return (
+        <>
+            <Divider size={'S'} marginY={'size-125'} />
+
+            <Flex gap={'size-125'}>
+                <div className={classes.iconContainer}>
+                    <Alert width={24} height={24} />
+                </div>
+
+                <Text>{message}</Text>
+            </Flex>
+        </>
+    );
+};
