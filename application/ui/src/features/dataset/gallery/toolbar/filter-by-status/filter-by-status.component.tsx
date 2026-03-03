737083c71541e1312b2e3f5b42475a1eec16b479
Joao Vilaca <joao.vilaca@intel.com>
2026-03-03 12:55:40 +0100

    Add filter by status component; Update EmptyDataset component

---
+++ application/ui/src/features/dataset/gallery/toolbar/filter-by-status/filter-by-status.component.tsx
@@ -0,0 +1,33 @@
+// Copyright (C) 2025-2026 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import { Item, Picker } from '@geti/ui';
+
+import { DatasetItemAnnotationStatus } from '../../../../../constants/shared-types';
+
+export type FilterByStatusKey = 'all' | DatasetItemAnnotationStatus;
+
+type FilterByStatusProps = {
+    onChange: (status: FilterByStatusKey | null) => void;
+};
+
+const FILTER_BY_STATUS_OPTIONS: { name: string; key: FilterByStatusKey }[] = [
+    { name: 'Status: All', key: 'all' },
+    { name: 'Status: Unannotated', key: 'unannotated' },
+    { name: 'Status: Reviewed', key: 'reviewed' },
+    { name: 'Status: To Review', key: 'to_review' },
+    { name: 'Status: Reviewed or Unannotated', key: 'reviewed_or_unannotated' },
+];
+
+export const FilterByStatus = ({ onChange }: FilterByStatusProps) => {
+    return (
+        <Picker
+            maxWidth='size-3000'
+            aria-label={'media status'}
+            items={FILTER_BY_STATUS_OPTIONS}
+            onSelectionChange={(status) => onChange(status as FilterByStatusKey | null)}
+        >
+            {(item) => <Item>{item.name}</Item>}
+        </Picker>
+    );
+};
