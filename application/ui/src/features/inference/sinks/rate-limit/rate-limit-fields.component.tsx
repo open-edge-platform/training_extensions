ea4e22fd3827b389f8606b4177192ba34cf74e69
Joao Vilaca <joao.vilaca@intel.com>
2026-03-04 15:45:16 +0100

    Add RateLimitFields

---
+++ application/ui/src/features/inference/sinks/rate-limit/rate-limit-fields.component.tsx
@@ -0,0 +1,23 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import { Flex, NumberField, Text } from '@geti/ui';
+
+import { positiveNumberOrUndefined } from '../utils';
+
+type RateLimitFieldsProps = {
+    rateLimit: number | null | undefined;
+};
+
+export const RateLimitFields = ({ rateLimit }: RateLimitFieldsProps) => {
+    const samples = positiveNumberOrUndefined(rateLimit);
+    const seconds = samples !== undefined ? 1 : undefined;
+
+    return (
+        <Flex direction='row' gap='size-100' alignItems={'end'}>
+            <NumberField label='Samples' name='rate_limit_samples' minValue={1} step={1} defaultValue={samples} />
+            <Text>every</Text>
+            <NumberField label='Seconds' name='rate_limit_seconds' minValue={0.1} step={0.1} defaultValue={seconds} />
+        </Flex>
+    );
+};
