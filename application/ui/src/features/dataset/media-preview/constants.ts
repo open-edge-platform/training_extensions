adf6ad61316d46f962e958c6720247cd0d19c507
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-03-04 16:41:57 +0100

    chore: Add section with border around sidebar in the annotator

--- application/ui/src/features/dataset/media-preview/constants.ts
+++ application/ui/src/features/dataset/media-preview/constants.ts
@@ -1,5 +1,5 @@
 // Copyright (C) 2025 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-export const SIDEBAR_WIDTH = 182;
+export const SIDEBAR_WIDTH = 198;
 export const SIDEBAR_MEDIA_SIZE = 150;
