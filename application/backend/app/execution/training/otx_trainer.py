002a3c3fec9e866e697570ae96b7008b8967297e
Aurélien Adriaenssens <aurelien.adriaenssens@intel.com>
2026-03-09 12:03:43 +0100

    Reverted changed to work_dir path.

--- application/backend/app/execution/training/otx_trainer.py
+++ application/backend/app/execution/training/otx_trainer.py
@@ -384,7 +384,7 @@ class OTXTrainer(Execution[TrainingJobParams]):
             model=otx_model,
             data=otx_datamodule,
             checkpoint=weights_path if has_parent_revision else None,
-            work_dir=str(Path(f"./otx-workspace-{model_id}").resolve()),
+            work_dir=f"./otx-workspace-{model_id}",
             device=otx_device_type,
         )
 
