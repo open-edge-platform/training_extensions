38b1fdc382c432a2c4c500690822c36dd724af29
Joao Vilaca <joao.vilaca@intel.com>
2026-03-12 15:35:40 +0100

    Update error message

--- application/ui/src/features/annotator/tools/segment-anything-tool/segment-anything-tool.component.tsx
+++ application/ui/src/features/annotator/tools/segment-anything-tool/segment-anything-tool.component.tsx
@@ -142,7 +142,8 @@ export const SegmentAnythingTool = () => {
     if (isError) {
         toast({
             type: 'error',
-            message: `Error in Segment Anything tool: ${error?.message ?? 'Unknown error'}`,
+            message: `
+            Error in Segment Anything tool: ${error?.message ?? 'Unknown error, please try refreshing the page.'}`,
         });
     }
 
