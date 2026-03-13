2bba963f2961e60426f9139a7cf8a38c45753665
Joao Vilaca <joao.vilaca@intel.com>
2026-03-13 14:43:05 +0100

    Add general spec file to test random stuff, for now, just toast tests

---
+++ application/ui/tests/general.spec.ts
@@ -0,0 +1,67 @@
+// Copyright (C) 2025-2026 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import { Page } from '@playwright/test';
+import { HttpResponse } from 'msw';
+
+import { getMockedProject } from '../mocks/mock-project';
+import { expect, http, test } from './fixtures';
+
+const expectToastToBeInteractive = async (page: Page, message: string) => {
+    const toastMessage = page.getByText(message);
+
+    await expect(toastMessage).toBeVisible();
+    await toastMessage.click();
+};
+
+test.describe('Toast Rendering', () => {
+    test('shows toast from create-project form context', async ({ page }) => {
+        await page.goto('/projects/new');
+
+        await page.getByLabel('detection', { exact: true }).click();
+        await page.getByRole('textbox', { name: 'Create label input' }).fill('Object');
+        await page.getByRole('button', { name: /Create label/i }).click();
+        await page.getByRole('button', { name: /delete label object/i }).click();
+
+        await expectToastToBeInteractive(page, 'At least one object is required');
+    });
+
+    test('shows toast from dialog context', async ({ page, network }) => {
+        network.use(
+            http.get('/api/projects', () => {
+                return HttpResponse.json([
+                    getMockedProject({ id: 'id-1', name: 'Project 1' }),
+                    getMockedProject({ id: 'id-2', name: 'Project 2' }),
+                ]);
+            })
+        );
+
+        await page.goto('/projects');
+
+        await page.getByTestId('id-1').click();
+        await page.getByText(/Delete/).click();
+        await page.getByRole('button', { name: 'Delete' }).click();
+
+        await expectToastToBeInteractive(page, 'Project deleted successfully');
+    });
+
+    test('shows toast from inference page context', async ({ page, network }) => {
+        network.use(
+            http.get('/api/projects/{project_id}/pipeline', () => {
+                return HttpResponse.json({
+                    project_id: 'id-1',
+                    status: 'running',
+                    source: null,
+                    sink: null,
+                    model: null,
+                    device: 'cpu',
+                });
+            })
+        );
+
+        await page.goto('/projects/id-1/inference');
+        await page.getByRole('button', { name: 'Disable Pipeline' }).click();
+
+        await expectToastToBeInteractive(page, 'Pipeline disabled successfully');
+    });
+});
