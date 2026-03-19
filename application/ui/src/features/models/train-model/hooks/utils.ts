c457a3241f82c2117d0f90e1ff7f30551068b2b0
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-03-19 14:24:46 +0100

    implement getTrainingConfigurationUpdatePayload

---
+++ application/ui/src/features/models/train-model/hooks/utils.ts
@@ -0,0 +1,42 @@
+// Copyright (C) 2025-2026 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import { isEmpty } from 'lodash-es';
+
+import type { TrainingConfiguration, TrainingConfigurationParameter } from '../../../../constants/shared-types';
+import { isParameterGroup } from '../../model-listing/model-training-parameters/utils';
+
+const collectConfigurationParameters = (
+    parameters: TrainingConfigurationParameter[],
+    prefixKey: string
+): Record<string, unknown> => {
+    return parameters.reduce<Record<string, unknown>>((acc, parameter) => {
+        if (isParameterGroup(parameter)) {
+            const newPrefix = isEmpty(prefixKey) ? parameter.key : prefixKey + '.' + parameter.key;
+            const result = collectConfigurationParameters(parameter.parameters, newPrefix);
+
+            return {
+                ...acc,
+                ...result,
+            };
+        } else {
+            const finalKey = isEmpty(prefixKey) ? parameter.key : prefixKey + '.' + parameter.key;
+            acc[finalKey] = parameter.value;
+        }
+        return acc;
+    }, {});
+};
+
+export const getTrainingConfigurationUpdatePayload = (
+    config: TrainingConfiguration | undefined
+): Record<string, unknown> => {
+    if (!config) {
+        return {};
+    }
+
+    if (config.parameters.length === 0) {
+        return {};
+    }
+
+    return collectConfigurationParameters(config.parameters, '');
+};
