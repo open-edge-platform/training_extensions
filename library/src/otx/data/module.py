d5ad5a73e4e4c30a13c2c849c1d47c4339d4d53b
kprokofi <kirill.prokofiev@intel.com>
2026-03-27 23:47:13 +0900

    return prefetch back

--- library/src/otx/data/module.py
+++ library/src/otx/data/module.py
@@ -421,7 +421,7 @@ class OTXDataModule(LightningDataModule):
             "persistent_workers": config.num_workers > 0,
             "sampler": sampler,
             "shuffle": sampler is None,
-            "prefetch_factor": 4 if config.num_workers > 0 else None,
+            "prefetch_factor": 2 if config.num_workers > 0 else None,
             "multiprocessing_context": _MP_CONTEXT if config.num_workers > 0 else None,
         }
 
