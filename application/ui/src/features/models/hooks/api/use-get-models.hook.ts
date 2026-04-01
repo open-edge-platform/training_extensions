a36bd86cace783681fb3e90c192d7ae0e2fd54fb
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-04-01 16:48:00 +0200

    chore: Use successful models in the training dialog (#6018)

--- application/ui/src/features/models/hooks/api/use-get-models.hook.ts
+++ application/ui/src/features/models/hooks/api/use-get-models.hook.ts
@@ -1,10 +1,11 @@
-// Copyright (C) 2025 Intel Corporation
+// Copyright (C) 2025-2026 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
 import { usePrefetchQuery, useSuspenseQuery } from '@tanstack/react-query';
 import { useProjectIdentifier } from 'hooks/use-project-identifier.hook';
 
 import { $api } from '../../../../api/client';
+import { isSuccessfulModel } from '../../model-listing/utils/utils';
 
 const getModelsQueryOptions = (projectId: string) => {
     return $api.queryOptions('get', '/api/projects/{project_id}/models', {
@@ -16,6 +17,15 @@ export const useGetModels = () => {
     const projectId = useProjectIdentifier();
 
     return useSuspenseQuery(getModelsQueryOptions(projectId));
+};
+
+export const useGetSuccessfulModels = () => {
+    const projectId = useProjectIdentifier();
+
+    return useSuspenseQuery({
+        ...getModelsQueryOptions(projectId),
+        select: (models) => models.filter((model) => isSuccessfulModel(model) && !model.files_deleted),
+    });
 };
 
 export const usePrefetchModels = () => {
