bac150b128a54c95120f403f57aa0a85de7fb69f
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-04-08 16:05:00 +0200

    test: test step

--- application/ui/src/features/models/train-model/advanced-settings/components/number-parameter-field.component.tsx
+++ application/ui/src/features/models/train-model/advanced-settings/components/number-parameter-field.component.tsx
@@ -6,17 +6,7 @@ import { useRef, useState } from 'react';
 import { Flex, NumberField, Slider } from '@geti/ui';
 
 import { NumberConfigurableParameter } from '../../../../../constants/shared-types';
-
-const getDecimalPoints = (value: number): number => {
-    // When log10 returns 0 (log10(1) = 0) we need to return 1
-    return Math.abs(Math.ceil(Math.log10(value))) || 1;
-};
-
-const getFloatingPointStep = (minValue: number, maxValue: number): number => {
-    const exponent = getDecimalPoints(maxValue - minValue);
-
-    return 1 / Math.pow(10, exponent + 3);
-};
+import { getStep } from './utils';
 
 type NumberGroupParamsProps = {
     name: string;
@@ -27,35 +17,6 @@ type NumberGroupParamsProps = {
     maxValue: number | null;
     value: number;
     type: NumberConfigurableParameter['value_type'];
-};
-
-const DEFAULT_INT_STEP = 1;
-const DEFAULT_FLOAT_STEP = 0.1;
-
-export const getStep = ({
-    step,
-    maxValue,
-    minValue,
-    type,
-}: {
-    step?: number;
-    minValue: number | null;
-    maxValue: number | null;
-    type: NumberConfigurableParameter['value_type'];
-}): number => {
-    if (step !== undefined) {
-        return step;
-    }
-
-    if (type === 'int') {
-        return DEFAULT_INT_STEP;
-    }
-
-    if (maxValue === null || minValue === null) {
-        return DEFAULT_FLOAT_STEP;
-    }
-
-    return getFloatingPointStep(minValue, maxValue);
 };
 
 export const NumberParameterField = ({
