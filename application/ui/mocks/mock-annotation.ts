321c0d73e5c7b5dc7c24ec82bff807871d8ef386
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-04-09 16:36:27 +0200

    test: useWithCancel

--- application/ui/mocks/mock-annotation.ts
+++ application/ui/mocks/mock-annotation.ts
@@ -1,19 +1,24 @@
 // Copyright (C) 2025 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-import type { Annotation } from '../src/shared/types';
+import type { Annotation, Shape } from '../src/shared/types';
 import { getMockedLabel } from './mock-labels';
+
+export const getMockedShape = (shape: Partial<Shape> = {}): Shape => {
+    return {
+        ...shape,
+        type: 'rectangle',
+        x: 10,
+        y: 20,
+        width: 100,
+        height: 50,
+    };
+};
 
 export const getMockedAnnotation = (annotation?: Partial<Annotation>): Annotation => {
     return {
         id: 'annotation-1',
-        shape: {
-            type: 'rectangle',
-            x: 10,
-            y: 20,
-            width: 100,
-            height: 50,
-        },
+        shape: getMockedShape(),
         labels: [getMockedLabel()],
         ...annotation,
     };
