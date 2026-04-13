a75879c91a7d648ce9cefa2da156664a49521d71
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-04-13 08:57:51 +0200

    fix: A few fixes in models screen (#6061)

--- application/ui/src/features/models/model-listing/utils/sorting.ts
+++ application/ui/src/features/models/model-listing/utils/sorting.ts
@@ -4,11 +4,11 @@
 import dayjs from 'dayjs';
 import { orderBy } from 'lodash-es';
 
-import type { Model } from '../../../../constants/shared-types';
+import type { DatasetRevision, Model } from '../../../../constants/shared-types';
 import { getTestingMetric } from '../components/model-row/utils';
 import type { SortBy } from '../types';
 
-export const sortModels = (models: Model[], sortBy: SortBy): Model[] => {
+export const sortModels = (models: Model[], sortBy: SortBy, datasetRevisions: DatasetRevision[]): Model[] => {
     switch (sortBy) {
         case 'name':
             return orderBy(models, (model) => model.name?.toLowerCase() ?? '', 'asc');
@@ -28,7 +28,37 @@ export const sortModels = (models: Model[], sortBy: SortBy): Model[] => {
             return orderBy(models, (model) => model.size ?? 0, 'asc');
         case 'score':
             return orderBy(models, (model) => getTestingMetric(model)?.value ?? 0, 'asc');
+        case 'dataset':
+            const datasetRevisionsMap = new Map(
+                datasetRevisions.map((datasetRevision) => [datasetRevision.id, datasetRevision.name])
+            );
+            return orderBy(
+                models,
+                [
+                    // First we sort by models that have dataset revision so they are on the top.
+                    (model) => {
+                        const name =
+                            model.training_info?.dataset_revision_id != null
+                                ? datasetRevisionsMap.get(model.training_info.dataset_revision_id)
+                                : undefined;
+
+                        return name != null ? 0 : 1;
+                    },
+                    // Then we sort alphabetically by dataset name
+                    (model) => {
+                        if (model.training_info?.dataset_revision_id != null) {
+                            return (
+                                datasetRevisionsMap.get(model.training_info.dataset_revision_id)?.toLowerCase() ?? ''
+                            );
+                        }
+
+                        return '';
+                    },
+                ],
+                ['asc', 'asc']
+            );
         default:
+            console.error(`Unknown sort option: ${sortBy satisfies never}`);
             return models;
     }
 };
