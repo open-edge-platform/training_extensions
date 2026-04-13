9e1680ddde9951a0d028c916eb57ab48fd02e500
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-04-13 08:57:01 +0200

    fix getMockedShape

--- application/ui/mocks/mock-annotation.ts
+++ application/ui/mocks/mock-annotation.ts
@@ -4,15 +4,14 @@
 import type { Annotation, Shape } from '../src/shared/types';
 import { getMockedLabel } from './mock-labels';
 
-export const getMockedShape = (shape: Partial<Shape> = {}): Shape => {
-    return {
-        ...shape,
-        type: 'rectangle',
-        x: 10,
-        y: 20,
-        width: 100,
-        height: 50,
-    };
+const SHAPE_DEFAULTS = {
+    rectangle: { type: 'rectangle', x: 10, y: 20, width: 100, height: 50 },
+    polygon: { type: 'polygon', points: [] },
+    full_image: { type: 'full_image' },
+};
+
+export const getMockedShape = (shape: Partial<Shape> & Pick<Shape, 'type'> = { type: 'rectangle' }): Shape => {
+    return { ...SHAPE_DEFAULTS[shape.type], ...shape } as Shape;
 };
 
 export const getMockedAnnotation = (annotation?: Partial<Annotation>): Annotation => {
