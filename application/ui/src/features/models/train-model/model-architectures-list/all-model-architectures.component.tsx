ab36f398e2ab24920a456e883965387fe5e5197b
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-04-14 12:04:32 +0200

    chore: Sort models is available only in the show more mode

--- application/ui/src/features/models/train-model/model-architectures-list/all-model-architectures.component.tsx
+++ application/ui/src/features/models/train-model/model-architectures-list/all-model-architectures.component.tsx
@@ -1,10 +1,13 @@
 // Copyright (C) 2025 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
+import { useState } from 'react';
+
 import { Flex } from '@geti/ui';
 
 import type { ModelArchitecture as ModelArchitectureType } from '../../../../constants/shared-types';
-import { SORTING_HANDLERS, SortingOptions } from '../sort-model-architectures/utils';
+import { SortModelArchitectures } from '../sort-model-architectures/sort-model-architectures.component';
+import { SORT_OPTIONS, SORTING_HANDLERS, SortingOptions } from '../sort-model-architectures/utils';
 import { ModelArchitecture } from './model-architecture.component';
 import { ModelArchitecturesListLayout } from './model-architectures-list-layout/model-architectures-list-layout.component';
 
@@ -13,7 +16,6 @@ type AllModelArchitecturesProps = {
     modelArchitectures: ModelArchitectureType[];
     selectedModelArchitectureId: string | null;
     onSelectedModelArchitectureIdChange: (modelArchitectureId: string | null) => void;
-    sortBy: SortingOptions;
 };
 
 export const AllModelArchitectures = ({
@@ -21,12 +23,13 @@ export const AllModelArchitectures = ({
     modelArchitectures,
     onSelectedModelArchitectureIdChange,
     selectedModelArchitectureId,
-    sortBy,
 }: AllModelArchitecturesProps) => {
+    const [sortBy, setSortBy] = useState<SortingOptions>(SortingOptions.NAME_ASC);
     const sortedModelArchitectures = SORTING_HANDLERS[sortBy](modelArchitectures);
 
     return (
         <Flex direction={'column'} gap={'size-200'}>
+            <SortModelArchitectures sortBy={sortBy} onSort={setSortBy} items={SORT_OPTIONS} />
             <ModelArchitecturesListLayout
                 selectedModelArchitectureId={selectedModelArchitectureId}
                 onSelectedModelArchitectureIdChange={onSelectedModelArchitectureIdChange}
