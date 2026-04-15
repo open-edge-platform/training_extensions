f267a0fd8cc076fc4ac4bd83c9efd167bb3f528a
Dmitry Kalinin <dmitry.kalinin@intel.com>
2026-04-15 14:37:57 +0200

    Fixed styles
    Signed-off-by: Dmitry Kalinin <dmitry.kalinin@intel.com>

--- application/ui/src/features/license/license.component.tsx
+++ application/ui/src/features/license/license.component.tsx
@@ -4,6 +4,7 @@
 import { Button, Content, Divider, Flex, Heading, Link, Text, View } from '@geti/ui';
 
 import { useAcceptLicense } from './api/use-accept-license.hook';
+import styles from './license.module.scss';
 
 const LICENSE_LINKS = {
     intelSimplified: {
@@ -33,48 +34,45 @@ export const License = ({ platform }: LicenseProps) => {
     const appLicense = platform === 'windows' ? LICENSE_LINKS.intelSimplified : LICENSE_LINKS.apache2;
 
     return (
-        <Flex justifyContent={'center'} alignItems={'center'} height={'100vh'}>
-            <View
-                backgroundColor={'gray-50'}
-                padding={'size-400'}
-                borderRadius={'regular'}
-                maxWidth={'size-6000'}
-                width={'100%'}
-            >
-                <Heading level={2}>License Agreement</Heading>
-                <Divider marginY={'size-200'} size={'S'} />
-                <Content>
-                    <Text>By installing, using, or distributing this application, you acknowledge that:</Text>
-                    <ul>
-                        <li>you have read and understood the license terms at the links below;</li>
-                        <li>confirmed the linked terms govern the contents you seek to access and use; and</li>
-                        <li>accepted and agreed to the linked license terms.</li>
-                    </ul>
-                    <Text>License links</Text>
-                    <ul>
-                        <li>
+        <View UNSAFE_className={styles.licenseBackground} height={'100vh'}>
+            <Flex justifyContent={'center'} alignItems={'center'} height={'100%'}>
+                <View
+                    backgroundColor={'gray-50'}
+                    padding={'size-400'}
+                    borderRadius={'regular'}
+                    maxWidth={'size-6000'}
+                    width={'100%'}
+                >
+                    <Heading level={2}>License Agreement</Heading>
+                    <Divider marginY={'size-200'} size={'S'} />
+                    <Content>
+                        <Text>By installing, using, or distributing this application, you acknowledge that:</Text>
+                        <ul className={styles.list}>
+                            <li>You have read and understood the license terms at the links below;</li>
+                            <li>Confirmed the linked terms govern the contents you seek to access and use; and</li>
+                            <li>Accepted and agreed to the linked license terms.</li>
+                        </ul>
+                        <Flex direction={'column'} marginTop={'size-200'}>
                             <Link href={appLicense.href} target={'_blank'} rel={'noopener noreferrer'}>
                                 {appLicense.label}
                             </Link>
-                        </li>
-                        <li>
                             <Link href={LICENSE_LINKS.dinov2.href} target={'_blank'} rel={'noopener noreferrer'}>
                                 {LICENSE_LINKS.dinov2.label}
                             </Link>
-                        </li>
-                    </ul>
-                </Content>
-                <Flex justifyContent={'end'} marginTop={'size-300'}>
-                    <Button
-                        variant={'accent'}
-                        onPress={() => acceptLicense(undefined)}
-                        isPending={isAccepting}
-                        isDisabled={isAccepting}
-                    >
-                        Accept and continue
-                    </Button>
-                </Flex>
-            </View>
-        </Flex>
+                        </Flex>
+                    </Content>
+                    <Flex justifyContent={'end'} marginTop={'size-300'}>
+                        <Button
+                            variant={'accent'}
+                            onPress={() => acceptLicense(undefined)}
+                            isPending={isAccepting}
+                            isDisabled={isAccepting}
+                        >
+                            Accept and continue
+                        </Button>
+                    </Flex>
+                </View>
+            </Flex>
+        </View>
     );
 };
