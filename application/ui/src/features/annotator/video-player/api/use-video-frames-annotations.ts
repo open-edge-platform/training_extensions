b7ffb8d6f756ebc7e3bd34b2f1481173d60aa6e8
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-04-23 10:22:26 +0200

    fix: Prediction loading and buffering (#6210)

--- application/ui/src/features/annotator/video-player/api/use-video-frames-annotations.ts
+++ application/ui/src/features/annotator/video-player/api/use-video-frames-annotations.ts
@@ -1,12 +1,53 @@
 // Copyright (C) 2025-2026 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
+import { usePrefetchQuery, useQuery } from '@tanstack/react-query';
 import { useProjectIdentifier } from 'hooks/use-project-identifier.hook';
 
 import { $api } from '../../../../api/client';
 import { AnnotatedVideoFrame } from '../../../../constants/shared-types';
 import { useVideoPlayer } from '../video-player-provider.component';
 import { getVideoFrameRangeIndexes } from './utils';
+
+const useGetVideoFramesAnnotationsQueryOptions = ({
+    frameNumber,
+    frameSkip,
+}: {
+    frameSkip: number;
+    frameNumber: number;
+}) => {
+    const projectId = useProjectIdentifier();
+    const { videoFrame } = useVideoPlayer();
+
+    const frames = videoFrame.frame_count - 1;
+
+    const { startFrameIndex, endFrameIndex } = getVideoFrameRangeIndexes({ frames, frameSkip, frameNumber });
+
+    return $api.queryOptions('get', '/api/projects/{project_id}/dataset/media/{media_id}/frames', {
+        params: {
+            path: {
+                project_id: projectId,
+                media_id: videoFrame.id,
+            },
+            query: {
+                frame_index_from: startFrameIndex,
+                frame_index_to: endFrameIndex,
+            },
+        },
+    });
+};
+
+export const usePrefetchVideoFramesAnnotations = ({
+    frameNumber,
+    frameSkip,
+}: {
+    frameNumber: number;
+    frameSkip: number;
+}) => {
+    const queryOptions = useGetVideoFramesAnnotationsQueryOptions({ frameNumber, frameSkip });
+
+    return usePrefetchQuery(queryOptions);
+};
 
 export const useVideoFramesAnnotations = <T>({
     frameNumber,
@@ -17,33 +58,16 @@ export const useVideoFramesAnnotations = <T>({
     frameSkip: number;
     selector: (data: AnnotatedVideoFrame[]) => T;
 }) => {
-    const projectId = useProjectIdentifier();
-    const { videoFrame } = useVideoPlayer();
+    const queryOptions = useGetVideoFramesAnnotationsQueryOptions({
+        frameSkip,
+        frameNumber,
+    });
 
-    const frames = videoFrame.frame_count - 1;
-
-    const { startFrameIndex, endFrameIndex } = getVideoFrameRangeIndexes({ frames, frameSkip, frameNumber });
-
-    return $api.useQuery(
-        'get',
-        '/api/projects/{project_id}/dataset/media/{media_id}/frames',
-        {
-            params: {
-                path: {
-                    project_id: projectId,
-                    media_id: videoFrame.id,
-                },
-                query: {
-                    frame_index_from: startFrameIndex,
-                    frame_index_to: endFrameIndex,
-                },
-            },
-        },
-        {
-            select: selector,
-            // We invalidate cache manually when the user annotates a frame, so we can set an infinite stale time to
-            // avoid unnecessary refetches
-            staleTime: Infinity,
-        }
-    );
+    return useQuery({
+        ...queryOptions,
+        select: selector,
+        // We invalidate cache manually when the user annotates a frame, so we can set an infinite stale time to
+        // avoid unnecessary refetches
+        staleTime: Infinity,
+    });
 };
