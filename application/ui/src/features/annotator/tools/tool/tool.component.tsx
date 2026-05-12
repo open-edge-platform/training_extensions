d3a54dce0aaa6878a479335a87deb6660445666f
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-05-12 09:55:53 +0200

    feat: Add tooltips with descrption and gif for the tools

---
+++ application/ui/src/features/annotator/tools/tool/tool.component.tsx
@@ -0,0 +1,73 @@
+// Copyright (C) 2025-2026 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import { ActionButton, Flex, Heading, IllustratedMessage, Text, Tooltip, TooltipTrigger, View } from '@geti/ui';
+import { useHotkeys } from 'react-hotkeys-hook';
+
+import { IconWrapper } from '../../../../components/icon-wrapper/icon-wrapper.component';
+import type { ToolConfig, ToolType } from '../interface';
+
+import classes from './tool.module.scss';
+
+interface HotkeyProps {
+    hotkey: string;
+}
+
+export const Hotkey = ({ hotkey }: HotkeyProps) => {
+    return (
+        <View backgroundColor={'gray-200'} paddingX={'size-100'} paddingY={'size-50'}>
+            <Text UNSAFE_className={classes.hotkeyText}>{hotkey.toLocaleUpperCase()}</Text>
+        </View>
+    );
+};
+
+type DrawingToolsTooltipProps = {
+    tool: ToolConfig;
+};
+
+export const DrawingToolsTooltip = ({ tool }: DrawingToolsTooltipProps) => {
+    const { tooltip, hotkey, label } = tool;
+
+    return (
+        <IllustratedMessage>
+            <img className={classes.drawingToolsTooltipsImg} src={tooltip?.img} alt={label} />
+            <View UNSAFE_className={classes.drawingToolsTooltipsContent}>
+                <Flex alignItems={'center'} justifyContent={'space-between'} order={2}>
+                    <Heading UNSAFE_className={classes.drawingToolsTooltipsTitle}>{label}</Heading>
+                    {hotkey !== undefined && <Hotkey hotkey={hotkey} />}
+                </Flex>
+                <Text UNSAFE_className={classes.drawingToolsTooltipsDescription}>{tooltip?.description}</Text>
+            </View>
+        </IllustratedMessage>
+    );
+};
+
+interface ToolProps {
+    tool: ToolConfig;
+    activeTool: ToolType | null;
+    setActiveTool: (tool: ToolType) => void;
+    isDisabled?: boolean;
+}
+
+export const Tool = ({ tool, activeTool, setActiveTool, isDisabled }: ToolProps) => {
+    useHotkeys(tool.hotkey, () => setActiveTool(tool.type), [setActiveTool, isDisabled], { enabled: !isDisabled });
+
+    return (
+        <TooltipTrigger placement={'right'}>
+            <ActionButton
+                isQuiet
+                width={'size-400'}
+                onPress={() => setActiveTool(tool.type)}
+                aria-label={`${tool.type} tool`}
+                isDisabled={isDisabled}
+            >
+                <IconWrapper isSelected={activeTool === tool.type} isDisabled={isDisabled}>
+                    <tool.icon data-tool={tool.type} />
+                </IconWrapper>
+            </ActionButton>
+            <Tooltip UNSAFE_className={tool.tooltip ? classes.drawingToolsTooltips : ''}>
+                {tool.tooltip === undefined ? `${tool.label} (${tool.hotkey})` : <DrawingToolsTooltip tool={tool} />}
+            </Tooltip>
+        </TooltipTrigger>
+    );
+};
--- application/ui/src/features/annotator/tools/tools.component.tsx
+++ application/ui/src/features/annotator/tools/tools.component.tsx
@@ -1,42 +1,10 @@
-// Copyright (C) 2025 Intel Corporation
+// Copyright (C) 2025-2026 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-import { ActionButton, Tooltip, TooltipTrigger } from '@geti/ui';
-import { useHotkeys } from 'react-hotkeys-hook';
-import { Fragment } from 'react/jsx-runtime';
+import { Fragment } from 'react';
 
-import { IconWrapper } from '../../../components/icon-wrapper/icon-wrapper.component';
 import type { ToolConfig, ToolType } from './interface';
-
-interface ToolProps {
-    tool: ToolConfig;
-    activeTool: ToolType | null;
-    setActiveTool: (tool: ToolType) => void;
-    isDisabled?: boolean;
-}
-
-const Tool = ({ tool, activeTool, setActiveTool, isDisabled }: ToolProps) => {
-    useHotkeys(tool.hotkey, () => setActiveTool(tool.type), [setActiveTool, isDisabled], { enabled: !isDisabled });
-
-    return (
-        <TooltipTrigger placement={'right'}>
-            <ActionButton
-                isQuiet
-                width={'size-400'}
-                onPress={() => setActiveTool(tool.type)}
-                aria-label={`${tool.type} tool`}
-                isDisabled={isDisabled}
-            >
-                <IconWrapper isSelected={activeTool === tool.type} isDisabled={isDisabled}>
-                    <tool.icon data-tool={tool.type} />
-                </IconWrapper>
-            </ActionButton>
-            <Tooltip>
-                {tool.label} ({tool.hotkey})
-            </Tooltip>
-        </TooltipTrigger>
-    );
-};
+import { Tool } from './tool/tool.component';
 
 interface ToolsProps {
     tools: ToolConfig[];
