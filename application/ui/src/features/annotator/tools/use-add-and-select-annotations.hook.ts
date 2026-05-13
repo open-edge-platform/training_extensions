d5c3c5f800e420ab4e35182cacfd688436b97e28
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-05-13 13:09:10 +0200

    refactor: update condition a bit

--- application/ui/src/features/annotator/tools/use-add-and-select-annotations.hook.ts
+++ application/ui/src/features/annotator/tools/use-add-and-select-annotations.hook.ts
@@ -3,6 +3,8 @@
 
 import { useCallback } from 'react';
 
+import { isEmpty } from 'lodash-es';
+
 import type { Label } from '../../../constants/shared-types';
 import { useAnnotationActions } from '../../../shared/annotator/annotation-actions-provider.component';
 import { useSelectedAnnotations } from '../../../shared/annotator/select-annotation-provider.component';
@@ -15,9 +17,11 @@ export const useAddAndSelectAnnotations = () => {
     const addAndSelectAnnotations = useCallback(
         (shapes: Shape[], labels: Label[]): string[] => {
             // If there is a global annotation, and we are trying to add new annotations,
-            // delete that annotation first
-            if (annotations.some((annotation) => annotation.shape.type === 'full_image')) {
-                deleteAnnotations(annotations.map(({ id }) => id));
+            // delete that global annotation first
+            const globalAnnotations = annotations.filter((annotation) => annotation.shape.type === 'full_image');
+
+            if (!isEmpty(globalAnnotations)) {
+                deleteAnnotations(globalAnnotations.map(({ id }) => id));
             }
 
             const newIds = addAnnotations(shapes, labels);
