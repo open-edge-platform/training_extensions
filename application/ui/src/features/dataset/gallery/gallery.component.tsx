12569d9ae0bc0371703b27c09360158508f5091d
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-05-27 16:22:07 +0200

    chore: select the media only when checkbox is clicked - fixes issue with select/unselect when double click

--- application/ui/src/features/dataset/gallery/gallery.component.tsx
+++ application/ui/src/features/dataset/gallery/gallery.component.tsx
@@ -59,7 +59,7 @@ const GalleryList = ({
     isMediaItemReviewedById,
 }: GalleryListProps) => {
     const projectId = useProjectIdentifier();
-    const { selectedKeys, setSelectedKeys, toggleSelectedKeys } = useSelectedData();
+    const { selectedKeys, toggleSelectedKeys } = useSelectedData();
 
     const isSetSelectedKeys = selectedKeys instanceof Set;
 
@@ -73,7 +73,6 @@ const GalleryList = ({
             isPending={isPending}
             isLoadingMore={isFetchingNextPage}
             onLoadMore={fetchNextPage}
-            onSelectionChange={setSelectedKeys}
             contentItem={(item) => {
                 const mediaUrl = getThumbnailUrl(projectId, item.id);
                 const downloadUrl = getMediaDownloadUrl(projectId, item.id);
