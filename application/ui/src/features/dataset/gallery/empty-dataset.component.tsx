12da04901c430deea7419d3af8d23d33e5ae83fd
Joao Vilaca <joao.vilaca@intel.com>
2026-05-29 16:28:44 +0200

    Add media upload button for empty dataset scenario

--- application/ui/src/features/dataset/gallery/empty-dataset.component.tsx
+++ application/ui/src/features/dataset/gallery/empty-dataset.component.tsx
@@ -1,9 +1,10 @@
 // Copyright (C) 2025-2026 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-import { Flex, Heading } from '@geti/ui';
+import { Content, Flex, Heading } from '@geti/ui';
 
 import { ReactComponent as EmptyDatasetImage } from '../../../assets/empty-dataset.svg';
+import { MediaUpload } from './toolbar/media-upload.component';
 
 type EmptyDatasetProps = {
     hasActiveFilter: boolean;
@@ -12,11 +13,20 @@ export const EmptyDataset = ({ hasActiveFilter }: EmptyDatasetProps) => {
     return (
         <Flex direction={'column'} gap={'size-200'} alignItems={'center'} justifyContent={'center'} height={'100%'}>
             <EmptyDatasetImage />
-            <Heading level={2}>
-                {hasActiveFilter
-                    ? 'No media items match your filter. Remove or select a new filter.'
-                    : 'Your dataset is empty. Upload your first media item to get started.'}
+            <Heading level={2} UNSAFE_style={{ textAlign: 'center' }}>
+                {hasActiveFilter ? (
+                    'No media items match your filter. Remove or select a new filter.'
+                ) : (
+                    <>
+                        Your dataset is empty.
+                        <br />
+                        Upload your first media item to get started.
+                    </>
+                )}
             </Heading>
+            <Content>
+                <MediaUpload />
+            </Content>
         </Flex>
     );
 };
