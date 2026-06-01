6ebbb3a8715ac9874f42dc179d7b34f7fd0a0a10
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-06-01 13:49:42 +0200

    remove duplicated header

--- application/ui/src/layout.tsx
+++ application/ui/src/layout.tsx
@@ -1,9 +1,6 @@
 // Copyright (C) 2025-2026 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-// Copyright (C) 2025 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-
 import { Suspense } from 'react';
 
 import { Flex, Grid, Item, Loading, TabList, Tabs, View } from '@geti/ui';
