24ef4aee0ed17aa29878d28baa1728551a5c3c3b
Camilo <camilo.colorado@intel.com>
2026-06-03 14:12:33 +0200

    fix: Valid media formats notification (#6635)

--- application/ui/src/features/dataset/gallery/drop-zone.component.tsx
+++ application/ui/src/features/dataset/gallery/drop-zone.component.tsx
@@ -10,40 +10,27 @@ import {
     Heading,
     IllustratedMessage,
     Text,
-    toast,
     View,
     type SpectrumDropZoneProps,
 } from '@geti/ui';
 
 import { ReactComponent as DropFiles } from '../../../assets/drop-files.svg';
 import { getFilesFromDropEvent } from '../../../shared/drop-zone.utils';
-import { isSupportedMediaFile } from './utils';
 
 import classes from './drop-zone.component.module.scss';
 
 type DatasetDropZoneProps = {
     children: ReactNode;
-    onFilesDropped?: (files: File[]) => void | Promise<void>;
+    onFilesDropped: (files: File[]) => void | Promise<void>;
 };
 
 type DropEvent = Parameters<NonNullable<SpectrumDropZoneProps['onDrop']>>[0];
 
 export const DatasetDropZone = ({ children, onFilesDropped }: DatasetDropZoneProps) => {
     const handleDrop = async (event: DropEvent) => {
-        if (onFilesDropped === undefined) {
-            return;
-        }
-
         const files = await getFilesFromDropEvent(event);
-        const supported = files.filter(isSupportedMediaFile);
 
-        if (supported.length < files.length) {
-            toast({ type: 'neutral', message: 'Some files were skipped because their format is not supported.' });
-        }
-
-        if (supported.length > 0) {
-            void onFilesDropped(supported);
-        }
+        onFilesDropped(files);
     };
 
     return (
