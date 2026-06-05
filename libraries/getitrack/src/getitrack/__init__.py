57cc7a38b6d3fd64c6de7ba9ecbe7a4ef802939c
Omkar Kabde <omkarkabde@gmail.com>
2026-06-05 14:31:15 +0530

    update license year

--- libraries/getitrack/src/getitrack/__init__.py
+++ libraries/getitrack/src/getitrack/__init__.py
@@ -1,5 +1,5 @@
-# Copyright (C) 2024 Intel Corporation
+# Copyright (C) 2026 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-"""getitrack: Multi-object tracking toolkit for getitune."""
+"""getitrack: Multi-object tracking toolkit."""
 
 __version__ = "0.1.0"
