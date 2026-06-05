3f0ac647109e05082f96cfe564acd5509d106dfe
João Vilaça <joao.vilaca@intel.com>
2026-06-05 12:30:56 +0200

    Disable SSIM (#6698)

--- application/ui/src/shared/hotkeys-definition.ts
+++ application/ui/src/shared/hotkeys-definition.ts
@@ -22,7 +22,6 @@ export const HOTKEYS = {
     autoSegmentation: 's',
     polygonTool: 'p',
     magneticLassoTool: 'm',
-    ssimTool: 'i',
     selectAllAnnotations: `${CTRL_OR_COMMAND_KEY}+a`,
     deselectAllAnnotations: `${CTRL_OR_COMMAND_KEY}+d`,
     submit: `${CTRL_OR_COMMAND_KEY}+s`,
@@ -53,7 +52,6 @@ export const TASK_HOTKEYS = {
         boundingBoxTool: HOTKEYS.boundingBoxTool,
         polygonTool: HOTKEYS.polygonTool,
         magneticLassoTool: HOTKEYS.magneticLassoTool,
-        ssimTool: HOTKEYS.ssimTool,
         ...COMMON_HOTKEYS,
         ...SELECTION_TOOL_HOTKEY,
         ...AUTO_SEGMENTATION_HOTKEY,
@@ -61,7 +59,6 @@ export const TASK_HOTKEYS = {
     instance_segmentation: {
         polygonTool: HOTKEYS.polygonTool,
         magneticLassoTool: HOTKEYS.magneticLassoTool,
-        ssimTool: HOTKEYS.ssimTool,
         ...COMMON_HOTKEYS,
         ...SELECTION_TOOL_HOTKEY,
         ...AUTO_SEGMENTATION_HOTKEY,
