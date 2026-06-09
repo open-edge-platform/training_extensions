b2c64f39930f1bff2152cf52560783c000338d1d
Albert van Houten <albert.van.houten@intel.com>
2026-06-09 11:20:29 +0200

    Revert KP changes
    Signed-off-by: Albert van Houten <albert.van.houten@intel.com>

--- library/src/getitune/data/dataset/keypoint_detection.py
+++ library/src/getitune/data/dataset/keypoint_detection.py
@@ -57,7 +57,7 @@ class KeypointDetectionDataset(VisionDataset):
             transforms=transforms,
             max_refetch=max_refetch,
         )
-        labels = dm_subset.label_categories.labels  # type: ignore[missing-attribute]
+        labels = dm_subset.schema.attributes["keypoints"].categories.labels
         self.label_info = LabelInfo(
             label_names=list(labels),
             label_groups=[],
