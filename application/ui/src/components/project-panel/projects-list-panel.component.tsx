8ae87125ccb28ec7bb301c7a8ed8200720a7e517
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-06-11 13:01:03 +0200

    fix: Display only one project related dialog (#6757)

--- application/ui/src/components/project-panel/projects-list-panel.component.tsx
+++ application/ui/src/components/project-panel/projects-list-panel.component.tsx
@@ -23,11 +23,16 @@ import { useProjectIdentifier } from 'hooks/use-project-identifier.hook';
 import { partition } from 'lodash-es';
 import { useNavigate } from 'react-router';
 
+import { EnablePipelineBlockedDialog } from '../../components/enable-pipeline-blocked-dialog/enable-pipeline-blocked-dialog.component';
+import { DeleteProjectDialog } from '../../components/project-dialogs/delete-project-dialog.component';
+import { EditProjectNameDialog } from '../../components/project-dialogs/edit-project-name-dialog.component';
 import { paths } from '../../constants/paths';
-import { MenuActions } from '../../features/project/list/menu-actions/menu-actions.component';
+import { Project } from '../../constants/shared-types';
+import { ProjectActionsMenu } from '../../features/project/list/menu-actions/menu-actions.component';
 import { getProjectTypeTitle } from '../../features/project/list/util';
 import { ProjectThumbnail } from './project-thumbnail/project-thumbnail.component';
 import { ProjectsList } from './projects-list.component';
+import { useProjectActionsDialogStates } from './use-project-actions-dialog-states';
 
 import classes from './projects-list.module.scss';
 
@@ -90,7 +95,8 @@ export const ProjectsListPanel = () => {
     const projectId = useProjectIdentifier();
     const { data } = useProjects();
 
-    const [[selectedProject], otherProjects] = partition(data, (project) => project.id === projectId);
+    const [matchedProjects, otherProjects] = partition(data, (project) => project.id === projectId);
+    const selectedProject: Project | undefined = matchedProjects.at(0);
     const selectedProjectName = selectedProject?.name ?? '';
     const hasActivePipeline = Boolean(selectedProject?.active_pipeline);
 
@@ -98,80 +104,141 @@ export const ProjectsListPanel = () => {
 
     const taskType = getProjectTypeTitle(selectedProject?.task);
 
+    const {
+        projectActionMetadata,
+        deleteProject,
+        editProject,
+        closeEditProject,
+        closeEnablePipelineBlocked,
+        enablePipelineBlocked,
+        isDeleteProjectDialogOpen,
+        isEditProjectNameDialogOpen,
+        isProjectListOpen,
+        isEnableBlockedDialogOpen,
+        closeDeleteProject,
+        changeProjectListDialogState,
+        clearProjectActionMetadata,
+    } = useProjectActionsDialogStates();
+
     const handleDeleted = () => {
-        navigate(paths.project.index({}));
+        if (selectedProject?.id === projectActionMetadata?.projectId) {
+            navigate(paths.project.index({}));
+        }
+
+        clearProjectActionMetadata();
     };
 
     return (
-        <DialogTrigger type='popover' hideArrow>
-            <SelectedProjectButton name={selectedProjectName} id={projectId} isActive={hasActivePipeline} />
+        <>
+            <DialogTrigger
+                type='popover'
+                hideArrow
+                isOpen={isProjectListOpen}
+                onOpenChange={changeProjectListDialogState}
+            >
+                <SelectedProjectButton name={selectedProjectName} id={projectId} isActive={hasActivePipeline} />
 
-            <Dialog width={'size-4600'} UNSAFE_className={classes.dialog}>
-                {selectedProject !== undefined && (
-                    <Header>
-                        <Flex
-                            direction={'column'}
-                            justifyContent={'center'}
-                            width={'100%'}
-                            alignItems={'center'}
-                            UNSAFE_style={{
-                                padding: 'var(--spectrum-global-dimension-size-200)',
-                            }}
-                            gap={'size-100'}
-                        >
-                            <ProjectThumbnail
-                                // when selected project changes, we want to reset the project thumbnail
-                                key={selectedProject.id}
-                                project={selectedProject}
-                                height={'size-1000'}
-                                width={'size-1000'}
-                            />
-                            <View width={'100%'} position={'relative'}>
-                                <Flex direction={'column'} alignItems={'center'} gap={'size-50'}>
-                                    <Heading UNSAFE_className={classes.dialogProjectName} level={2} marginBottom={0}>
-                                        {selectedProjectName}
-                                    </Heading>
-
-                                    {taskType !== undefined && (
-                                        <Badge variant={'neutral'}>
-                                            <Text>{taskType}</Text>
-                                        </Badge>
-                                    )}
-                                </Flex>
-
-                                <MenuActions
-                                    projectId={selectedProject.id}
-                                    projectName={selectedProject.name}
-                                    isPipelineRunning={selectedProject.active_pipeline}
-                                    projectNames={otherProjectNames}
-                                    onDeleted={handleDeleted}
-                                    actionButtonStyle={{
-                                        position: 'absolute',
-                                        top: '50%',
-                                        right: dimensionValue('size-100'),
-                                        transform: 'translateY(-50%)',
-                                    }}
+                <Dialog width={'size-4600'} UNSAFE_className={classes.dialog}>
+                    {selectedProject !== undefined && (
+                        <Header>
+                            <Flex
+                                direction={'column'}
+                                justifyContent={'center'}
+                                width={'100%'}
+                                alignItems={'center'}
+                                UNSAFE_style={{
+                                    padding: 'var(--spectrum-global-dimension-size-200)',
+                                }}
+                                gap={'size-100'}
+                            >
+                                <ProjectThumbnail
+                                    // when selected project changes, we want to reset the project thumbnail
+                                    key={selectedProject.id}
+                                    project={selectedProject}
+                                    height={'size-1000'}
+                                    width={'size-1000'}
                                 />
-                            </View>
-                            {hasActivePipeline ? <Tag text={'Active'} /> : null}
-                        </Flex>
-                    </Header>
-                )}
+                                <View width={'100%'} position={'relative'}>
+                                    <Flex direction={'column'} alignItems={'center'} gap={'size-50'}>
+                                        <Heading
+                                            UNSAFE_className={classes.dialogProjectName}
+                                            level={2}
+                                            marginBottom={0}
+                                        >
+                                            {selectedProjectName}
+                                        </Heading>
 
-                {otherProjects.length > 0 && (
-                    <>
-                        <Divider size={'S'} marginBottom={'size-100'} marginTop={0} />
+                                        {taskType !== undefined && (
+                                            <Badge variant={'neutral'}>
+                                                <Text>{taskType}</Text>
+                                            </Badge>
+                                        )}
+                                    </Flex>
 
-                        <Content margin={0}>
-                            <ProjectsList projects={otherProjects} />
-                        </Content>
-                    </>
-                )}
+                                    <ProjectActionsMenu
+                                        projectId={selectedProject.id}
+                                        projectName={selectedProject.name}
+                                        isPipelineRunning={selectedProject.active_pipeline}
+                                        projectNames={otherProjectNames}
+                                        onRename={editProject}
+                                        onDelete={deleteProject}
+                                        onEnableBlocked={enablePipelineBlocked}
+                                        actionButtonStyle={{
+                                            position: 'absolute',
+                                            top: '50%',
+                                            right: dimensionValue('size-100'),
+                                            transform: 'translateY(-50%)',
+                                        }}
+                                    />
+                                </View>
+                                {hasActivePipeline ? <Tag text={'Active'} /> : null}
+                            </Flex>
+                        </Header>
+                    )}
 
-                <ButtonGroup UNSAFE_className={classes.buttonsGroup}>
-                    <ManageProjects />
-                </ButtonGroup>
-            </Dialog>
-        </DialogTrigger>
+                    {otherProjects.length > 0 && (
+                        <>
+                            <Divider size={'S'} marginBottom={'size-100'} marginTop={0} />
+
+                            <Content margin={0}>
+                                <ProjectsList
+                                    projects={otherProjects}
+                                    onRename={editProject}
+                                    onDelete={deleteProject}
+                                    onEnableBlocked={enablePipelineBlocked}
+                                />
+                            </Content>
+                        </>
+                    )}
+
+                    <ButtonGroup UNSAFE_className={classes.buttonsGroup}>
+                        <ManageProjects />
+                    </ButtonGroup>
+                </Dialog>
+            </DialogTrigger>
+
+            {projectActionMetadata !== null && (
+                <EditProjectNameDialog
+                    key={`edit-${projectActionMetadata.projectId}`}
+                    projectId={projectActionMetadata.projectId}
+                    projectName={projectActionMetadata.projectName}
+                    projectNames={projectActionMetadata.projectNames}
+                    isOpen={isEditProjectNameDialogOpen}
+                    onClose={closeEditProject}
+                />
+            )}
+
+            {projectActionMetadata !== null && (
+                <DeleteProjectDialog
+                    projectId={projectActionMetadata.projectId}
+                    projectName={projectActionMetadata.projectName}
+                    isOpen={isDeleteProjectDialogOpen}
+                    onClose={closeDeleteProject}
+                    onDeleted={handleDeleted}
+                />
+            )}
+
+            <EnablePipelineBlockedDialog isOpen={isEnableBlockedDialogOpen} onClose={closeEnablePipelineBlocked} />
+        </>
     );
 };
