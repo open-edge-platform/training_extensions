24be4f2cc5b24a69774f3f303b8e9d63babaf3c4
kprokofi <kirill.prokofiev@intel.com>
2026-06-11 21:46:44 +0900

    update license in files

--- application/backend/app/api/routers/models.py
+++ application/backend/app/api/routers/models.py
@@ -116,7 +116,14 @@ def download_model_binary(
     model_variant = model_service.get_variant(variant_id=model_variant_id)
     filename = f"model-{str(model_id).split('-')[0]}-{model_variant.format}-{model_variant.precision}.zip"
 
-    demo_files = demo_files_service.build_demo_files(project_id=project.id, model_format=model_variant.format)
+    architecture = model_service.get_model_revision_architecture(project_id=project.id, model_id=model_id)
+    is_ultralytics = "yolo26" in architecture
+
+    demo_files = demo_files_service.build_demo_files(
+        project_id=project.id,
+        model_format=model_variant.format,
+        is_ultralytics=is_ultralytics,
+    )
 
     # Create an in-memory zip file
     zip_buffer = io.BytesIO()
