741faaf75fbd19ab06bc5ff1b7ce11948349b6c5
Omkar Kabde <omkarkabde@gmail.com>
2026-06-17 15:26:19 +0530

    update `__init__.py`

--- libraries/getitrack/src/getitrack/core/__init__.py
+++ libraries/getitrack/src/getitrack/core/__init__.py
@@ -1,3 +1,17 @@
 # Copyright (C) 2026 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 """Core data types and tracker interface."""
+
+from getitrack.core.base import ALGORITHM_REGISTRY, BaseTracker, register_algorithm
+from getitrack.core.detection import Detections, TrackedDetections, TrackState
+from getitrack.core.track import Track
+
+__all__ = [
+    "ALGORITHM_REGISTRY",
+    "BaseTracker",
+    "Detections",
+    "Track",
+    "TrackState",
+    "TrackedDetections",
+    "register_algorithm",
+]
