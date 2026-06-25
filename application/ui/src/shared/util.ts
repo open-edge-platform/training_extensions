ff17fe5aa8486dc47bc8d69a11f13865bce8bc4a
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-06-25 14:34:31 +0200

    improve assetIsNotNullable

--- application/ui/src/shared/util.ts
+++ application/ui/src/shared/util.ts
@@ -25,8 +25,8 @@ export const formatBytes = (bytes: number): string => prettyBytes(bytes);
 
 export const pluralizeItems = (count: number) => (pluralRules.select(count) === 'one' ? 'item' : 'items');
 
-export function assertIsNotNullable<T>(value: T | null | undefined): asserts value is T {
+export function assertIsNotNullable<T>(value: T | null | undefined, name = 'value'): asserts value is T {
     if (value === null || value === undefined) {
-        throw new Error(`${value} must not be null or undefined`);
+        throw new Error(`${name} must not be null or undefined`);
     }
 }
