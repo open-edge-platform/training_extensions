d7579aa562b07119162bbb2ee9b14cdc68d0b6e5
andersendsa <andersendsa631@gmail.com>
2026-06-25 09:40:39 +0530

    reverting changes

--- application/ui/src/components/zoom/use-container-size.ts
+++ application/ui/src/components/zoom/use-container-size.ts
@@ -1,40 +1,29 @@
 // Copyright (C) 2025 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-import { useState, useEffect, useCallback, type RefObject } from 'react';
+import { useState, type RefObject } from 'react';
 
 import { useResizeObserver } from '@react-aria/utils';
 
 export const useContainerSize = (ref: RefObject<HTMLElement | null>) => {
     const [size, setSize] = useState({ width: 100, height: 100 });
-
-    const updateSize = useCallback(() => {
-        if (!ref.current) {
-            return;
-        }
-
-        setSize((prevSize) => {
-            if (prevSize.width === ref.current!.clientWidth && prevSize.height === ref.current!.clientHeight) {
-                return prevSize;
-            }
-
-            return {
-                width: ref.current!.clientWidth,
-                height: ref.current!.clientHeight,
-            };
-        });
-    }, [ref]);
-
-    useEffect(() => {
-        updateSize();
-        window.addEventListener('resize', updateSize);
-        return () => window.removeEventListener('resize', updateSize);
-    }, [updateSize]);
-
     useResizeObserver({
         ref,
         box: 'border-box',
-        onResize: updateSize,
+        onResize: () => {
+            if (!ref.current) {
+                return;
+            }
+
+            if (size.width === ref.current.clientWidth && size.height === ref.current.clientHeight) {
+                return;
+            }
+
+            setSize({
+                width: ref.current.clientWidth,
+                height: ref.current.clientHeight,
+            });
+        },
     });
 
     return size;
