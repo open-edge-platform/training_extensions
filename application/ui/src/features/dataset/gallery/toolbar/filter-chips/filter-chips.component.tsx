a8d3f3eac8752f7c716f0fe8bfa8acc0e3e34d6d
Joao Vilaca <joao.vilaca@intel.com>
2026-07-01 16:23:25 +0200

    Address more comments

--- application/ui/src/features/dataset/gallery/toolbar/filter-chips/filter-chips.component.tsx
+++ application/ui/src/features/dataset/gallery/toolbar/filter-chips/filter-chips.component.tsx
@@ -1,7 +1,7 @@
 // Copyright (C) 2026 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-import { Flex, Text } from '@geti-ui/ui';
+import { ActionButton, Flex, Text } from '@geti-ui/ui';
 import { CloseSmall } from '@geti-ui/ui/icons';
 
 import classes from './filter-chips.module.scss';
@@ -12,26 +12,13 @@ type FilterChipsProps = {
 };
 
 export const FilterChips = ({ name, onClose }: FilterChipsProps) => {
-    const handleClose = () => {
-        onClose();
-    };
-
     return (
         <Flex UNSAFE_className={classes.container} alignItems={'center'} gap={'size-75'}>
             <Text UNSAFE_className={classes.name}>{name}</Text>
 
-            <CloseSmall
-                className={classes.closeIcon}
-                role={'button'}
-                tabIndex={0}
-                aria-label={`Remove ${name} filter`}
-                onClick={handleClose}
-                onKeyDown={(event) => {
-                    if (event.key === 'Enter' || event.key === ' ') {
-                        handleClose();
-                    }
-                }}
-            />
+            <ActionButton isQuiet aria-label={`Remove ${name} filter`} onPress={onClose}>
+                <CloseSmall />
+            </ActionButton>
         </Flex>
     );
 };
